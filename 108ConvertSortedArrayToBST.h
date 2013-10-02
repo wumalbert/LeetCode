b5e88cdb4ea2710fd57c8d0b3093fa47c162b94e
wumalbert <wumalbert@gmail.com>
2013-10-02 17:09:05 +0800

    change the interface

--- 108ConvertSortedArrayToBST.h
+++ 108ConvertSortedArrayToBST.h
@@ -7,22 +7,53 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution01 {
 private:
-    TreeNode *sortedArrayToBST(const vector<int>& num, int begin, int end)
+    // convert the [start, end) of the sorted array to BST
+    TreeNode *sortedArrayToBST(const vector<int> &num, int start, int end)
     {
-        if (begin >= end) return NULL;
-        
-        int middle = begin + (end-1-begin)/2;
-        TreeNode *root = new TreeNode(num[middle]);
-        root->left = sortedArrayToBST(num, begin, middle);
-        root->right = sortedArrayToBST(num, middle+1, end);
+        if (start >= end) return NULL;
+        int mid = start + ((end - 1 - start) >> 1);
+        TreeNode *root = new TreeNode(num[mid]);
+        root->left = sortedArrayToBST(num, start, mid);
+        root->right = sortedArrayToBST(num, mid+1, end);
         return root;
     }
 public:
     TreeNode *sortedArrayToBST(vector<int> &num) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        return sortedArrayToBST(num, 0, num.size());
+        return sortedArrayToBST(num, 0, num.size()); 
+    }
+};
+
+class Solution {
+private:
+    // convert the [start, end) of the sorted array to BST
+    TreeNode *sortedArrayToBST(vector<int>::const_iterator begin, vector<int>::const_iterator end)
+    {
+        if (begin >= end) return NULL;
+        vector<int>::const_iterator mid = begin + ((end - begin) >> 1);
+        TreeNode *root = new TreeNode(*mid);
+        root->left = sortedArrayToBST(begin, mid);
+        root->right = sortedArrayToBST(mid+1, end);
+        return root;
+    }
+public:
+    TreeNode *sortedArrayToBST(vector<int> &num) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        return sortedArrayToBST(num.begin(), num.end()); 
     }
 };
