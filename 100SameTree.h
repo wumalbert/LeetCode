ba60eadbb49b12fc00642c3eb563a8f273728536
wumalbert <wumalbert@gmail.com>
2013-10-02 22:35:41 +0800

    iteration solution

--- 100SameTree.h
+++ 100SameTree.h
@@ -7,15 +7,51 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+class Solution01 {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        
+        // pretraverse the tree
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        
+        stack<TreeNode*> treeStack1, treeStack2;
+        TreeNode *current1 = p, *current2 = q;
+        while(!treeStack1.empty() || current1)
+        {
+            if (current1)
+            {
+                if(!current2 || current2->val != current1->val) return false;
+                if(current1->right && current2->right)
+                {
+                    treeStack1.push(current1->right);
+                    treeStack2.push(current2->right);
+                }
+                else if ((!current1->right && current2->right) || (current1->right && !current2->right)) return false;
+                current1 = current1->left;
+                current2 = current2->left;
+            }
+            else
+            {
+                if (current2 || treeStack2.empty()) return false;
+                current1 = treeStack1.top();
+                treeStack1.pop();
+                current2 = treeStack2.top();
+                treeStack2.pop();
+            }
+        }
+        return true;
+    }
+};
+
 class Solution {
 public:
     bool isSameTree(TreeNode *p, TreeNode *q) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (!p && !q) return true; // both p and q are NULL
-        if (!p || !q) return false; // any ony of p and q is NULL
-        if (p->val != q->val) return false;
-        
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        if(p->val != q->val) return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
