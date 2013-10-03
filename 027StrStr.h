0ff299e180c37777797b2293e6aa0938a7b3adf2
wumalbert <wumalbert@gmail.com>
2013-10-03 17:46:23 +0800

    Bruce Force solution

--- 027StrStr.h
+++ 027StrStr.h
@@ -1,53 +1,54 @@
+// Bruce force solution
+class Solution01 {
+public:
+    char *strStr(char *haystack, char *needle) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!haystack || !needle) return NULL;
+        if (!*needle) return haystack;
+        while (*haystack) {
+            char *ch1 = haystack, *ch2 = needle;
+            while (*ch1 && *ch2 && *ch1 == *ch2) {
+                ++ch1;
+                ++ch2;
+            }
+            if (!*ch2) return haystack;
+            else if (!*ch1) return NULL;
+            else ++haystack;
+        }
+        return NULL;
+    }
+};
+// KMP algorithm
 class Solution {
 public:
     char *strStr(char *haystack, char *needle) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         if (!haystack || !needle) return NULL;
-        
-        // use KMP to find the substr
-        int needleLength = 0;
-        for (needleLength = 0; needle[needleLength] != '\0'; ++needleLength);
-        
-        if (needleLength <= 0) return haystack;
-        
-        int *back = new int[needleLength];
-        back[0] = -1;
-        
-        for (int i = 1; i < needleLength; ++i)
-        {
-            int j = back[i-1];
-            
-            while (j != -1 && needle[j] != needle[i-1])
-            {
-                j = back[j];
+        if (!*needle) return haystack;
+        int n = strlen(haystack), m = strlen(needle);
+        // build the next array, when i mismatch go to next[i]
+        vector<int> next(m, -1);
+        for (int i = 1; i < m; ++i) {
+            int j = next[i-1];
+            while (j != -1 && needle[i-1] != needle[j]) {
+                j = next[j];
             }
-            
-            if (j != -1) back[i] = j + 1;
-            else back[i] = 0;
+            next[i] = j+1;
         }
-        
         int i = 0, j = 0;
-        while (haystack[i] != '\0' && needle[j] != '\0')
-        {
-            if (haystack[i] == needle[j])
-            {
-                ++i;
+        while (i < n && j < m) {
+            if (needle[j] == haystack[i]) {
                 ++j;
-            }
-            else
-            {
-                j = back[j];
-                if (-1 == j)
-                {
+                ++i;
+            } else {
+                j = next[j];
+                if (-1 == j) {
                     ++i;
                     ++j;
                 }
             }
         }
-        
-        if ('\0' == needle[j]) return haystack + i - needleLength;
-        
-        return NULL;
+        if (j == m) return haystack + i - m;
+        else return NULL;
     }
 };
