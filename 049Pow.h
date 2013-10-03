4affff9368a0c165ebc75199441ffb02daf852fa
wumalbert <wumalbert@gmail.com>
2013-10-03 19:43:49 +0800

    iteration solution

--- 049Pow.h
+++ 049Pow.h
@@ -1,21 +1,34 @@
+// recursive solution
+class Solution01 {
+public:
+    double pow(double x, int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (0 == n) return 1;
+        else if (1 == n) return x;
+        static const int kNegetiveMask = 0x80000000;
+        bool negetive = n & kNegetiveMask;
+        unsigned int m = n;
+        double ret = pow(x, n>>1);
+        if (n & 0x1) ret *= ret*x;
+        else ret *= ret;
+        return negetive ? 1/ret : ret; 
+    }
+};
+// iteration solution
 class Solution {
 public:
     double pow(double x, int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (0 == n) return 1;
-        if (1 == n) return x;
-        
-        if (n < 0)
-        {
-            x = 1 / x;
-            n = -n;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        static const int kNegetiveMask = 0x80000000;
+        bool negetive = n & kNegetiveMask;
+        unsigned int m = n;
+        if (negetive) m = ~m + 1;
+        double ret = 1;
+        while (m) {
+            if (m & 0x1) ret *= x;
+            x *= x;
+            m >>= 1;
         }
-        
-        double r = pow(x, n/2);
-        
-        if (n&0x1) return r * r * x;
-        
-        return r*r;
+        return negetive ? 1/ret : ret;
     }
 };
