d597e51d7223b12385fb24053f3f8fea256b60ce
wumalbert <wumalbert@gmail.com>
2013-10-03 22:39:04 +0800

    code style optimization

--- 055MergeIntervals.h
+++ 055MergeIntervals.h
@@ -7,41 +7,25 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
-bool intervalComp(Interval i1, Interval i2)
-{
-    return i1.start < i2.start;
+bool MyComp(const Interval &a, const Interval &b) {
+    return a.start < b.start;
 }
-
 class Solution {
 public:
     vector<Interval> merge(vector<Interval> &intervals) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (intervals.size() <= 1) return intervals;
-        
-        sort(intervals.begin(), intervals.end(), intervalComp);
-        
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<Interval> ret;
-        
-        vector<Interval>::iterator it = intervals.begin();
-        int currStart = it->start;
-        int currEnd = it->end;
-        ++it;
-        while (it != intervals.end())
+        if (intervals.empty()) return ret;
+        sort(intervals.begin(), intervals.end(), MyComp);
+        ret.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); ++i)
         {
-            
-            if (it->start <= currEnd) currEnd = max(currEnd, it->end);
-            else
-            {
-                ret.push_back(Interval(currStart, currEnd));
-                
-                currStart = it->start;
-                currEnd = it->end;
+            if (intervals[i].start > ret.back().end) {
+                ret.push_back(intervals[i]);
+            } else if (intervals[i].end > ret.back().end) {
+                ret.back().end = intervals[i].end;
             }
-            ++it;
         }
-        ret.push_back(Interval(currStart, currEnd));
-        
         return ret;
     }
 };
--- 056InsertInterval.h
+++ 056InsertInterval.h
@@ -7,40 +7,68 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
+class Solution01 {
+public:
+    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int low = 0, high = intervals.size()-1;
+        // find the start_index for newInterval.start
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (newInterval.start <= intervals[mid].end) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        int start_index = low;
+        // find the end_index for newInterval.end
+        low = 0;
+        high = intervals.size()-1;
+        while (low <= high) {
+            int mid = low + (high-low >> 1);
+            if (newInterval.end < intervals[mid].start) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        int end_index = high;
+        
+        vector<Interval> ret(intervals.begin(), intervals.begin() + start_index);
+        if (start_index <= end_index) {
+            newInterval.start = min(newInterval.start, intervals[start_index].start);
+            newInterval.end = max(newInterval.end, intervals[end_index].end);
+        }
+        ret.push_back(newInterval);
+        ret.insert(ret.end(), intervals.begin()+end_index+1, intervals.end());
+        return ret;
+    }
+};
 class Solution {
 public:
     vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<Interval> ret;
-        
-        vector<Interval>::iterator it;
-        for (it = intervals.begin(); it != intervals.end() && it->end < newInterval.start; ++it)
-            ret.push_back(*it);
-        
-        if (it == intervals.end())
-        {
+        int i = 0;
+        for (i = 0; i < intervals.size() && intervals[i].end < newInterval.start; ++i) {
+            ret.push_back(intervals[i]);
+        }
+        if (intervals.size() == i) {
             ret.push_back(newInterval);
             return ret;
         }
-        
-        if (it->start > newInterval.end)
-        {
+        if (intervals[i].start > newInterval.end) {
+            ret.push_back(newInterval);
+        } else {
+            newInterval.start = min(intervals[i].start, newInterval.start);
+            for (++i; i < intervals.size() && intervals[i].start <= newInterval.end; ++i) {}
+            newInterval.end = max(intervals[i-1].end, newInterval.end);
             ret.push_back(newInterval);
         }
-        else
-        {
-            int start = min(newInterval.start, it->start);
-            for (; it != intervals.end() && it->start <= newInterval.end; ++it);
-            --it;
-            int end = max(newInterval.end, it->end);
-            ret.push_back(Interval(start, end));
-            ++it;
+        for (; i < intervals.size(); ++i) {
+            ret.push_back(intervals[i]);
         }
-        
-        for (; it != intervals.end(); ++it)
-            ret.push_back(*it);
-            
         return ret;
     }
 };
