57cf332f2e4092a373034e99b67f11699fb86f39
wumalbert <wumalbert@gmail.com>
2013-10-03 10:49:05 +0800

    code optimization

--- 125VaildPalindrome.h
+++ 125VaildPalindrome.h
@@ -1,33 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         if(s.empty()) return true;
-        int len = s.length();
-        
-        const char *begin = s.data(), *end = begin+(len-1);
-        while(begin < end)
+        string::const_iterator left = s.begin(), right = s.end()-1;
+        while(left < right)
         {
-            char c1 = *begin;
-            char c2 = *end;
-            if (!isalpha(c1) && !isdigit(c1))
+            if(!isalpha(*left) && !isdigit(*left)) ++left;
+            else if(!isalpha(*right) && !isdigit(*right)) --right;
+            else 
             {
-                ++begin;
-                continue;
+                char ch1 = isalpha(*left) ? tolower(*left) : *left;
+                char ch2 = isalpha(*right) ? tolower(*right) : *right;
+                if(ch1 == ch2)
+                {
+                    ++left;
+                    --right;
+                }
+                else return false;
             }
-            if (!isalpha(c2) && !isdigit(c2))
-            {
-                --end;
-                continue;
-            }
-            
-            if (isalpha(c1)) c1 = tolower(c1);
-            if (isalpha(c2)) c2 = tolower(c2);
-            if (c1 != c2) return false;
-            
-            ++begin;
-            --end;
         }
         return true;
     }
