151fe54c7c49472a3be4367def1662217eb95253
Min WU <wumalbert@gmail.com>
2013-10-04 13:09:49 +0800

    code style optimization

--- 073SetMatrixZeros.h
+++ 073SetMatrixZeros.h
@@ -1,75 +1,44 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int> > &matrix) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        bool firstRowHasZero = false;
-        bool firstColHasZero = false;
-        
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        // set the first row zero mask
-        for (int i = 0; i < n; ++i)
-        {
-            if (!matrix[0][i])
-            {
-                firstRowHasZero = true;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        bool first_row_mask = false, first_column_mask = false;  // indicate whether it should be set zero
+        int m = matrix.size(), n = matrix[0].size();
+        for (int j = 0; j < n; ++j) {
+            if (0 == matrix[0][j]) {
+                first_row_mask = true;
                 break;
             }
         }
-        
-        // set the first column zero mask
-        for (int i = 0; i < m; ++i)
-        {
-            if (!matrix[i][0])
-            {
-                firstColHasZero = true;
+        for (int i = 0; i < m; ++i) {
+            if (0 == matrix[i][0]) {
+                first_column_mask = true;
                 break;
             }
         }
-        
-        // store each row zero mask in the first element of the row (the first column)
-        // store each column zero mask in the first element of the column (the first row)
-        for (int i = 1; i < m; ++i)
-        {
-            for (int j = 1; j < n; ++j)
-            {
-                if (!matrix[i][j])
-                {
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (0 == matrix[i][j]) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
-        
-        // set the rows to zero
-        for (int i = 1; i < m; ++i)
-        {
-            if (!matrix[i][0])
-            {
-                for (int j = 1; j < n; ++j)
-                    matrix[i][j] = 0;
+        for (int i = 1; i < m; ++i) {
+            if (0 == matrix[i][0]) {
+                for (int j = 1; j < n; ++j) matrix[i][j] = 0;
             }
         }
-        
-        // set the columns to zero
-        for (int i = 1; i < n; ++i)
-        {
-            if (!matrix[0][i])
-            {
-                for (int j = 1; j < m; ++j)
-                    matrix[j][i] = 0;
+        for (int j = 1; j < n; ++j) {
+            if (0 == matrix[0][j]) {
+                for (int i = 1; i < m; ++i) matrix[i][j] = 0;
             }
         }
-        
-        // set the first row to zero
-        if (firstRowHasZero)
-            for (int i = 0; i < n; ++i)
-                matrix[0][i] = 0;
-                
-        if (firstColHasZero)
-            for (int i = 0; i < m; ++i)
-                matrix[i][0] = 0;
+        if (first_row_mask) {
+            for (int j = 0; j < n; ++j) matrix[0][j] = 0;
+        }
+        if (first_column_mask) {
+            for (int i = 0; i < m; ++i) matrix[i][0] = 0;
+        }
     }
 };
