66b445d49550f640bdd080e748e54fa95da7acbb
wumalbert <wumalbert@gmail.com>
2013-10-04 23:06:19 +0800

    code optimization with hashmap

--- 012IntToRoman.h
+++ 012IntToRoman.h
@@ -1,25 +1,15 @@
 class Solution {
-
 public:
     string intToRoman(int num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function    
-        string result;
-        
-        static const int basicInt[13] = {1, 4, 5, 9, 10, 40, 50, 
-            90, 100, 400, 500, 900, 1000};
-        static const string basicRoman[13] = {"I", "IV", "V", "IX", 
-            "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-        
-        for (int i = 12; i >= 0; --i)
-        {
-            int t = num / basicInt[i];
-            for (int j = 0; j < t; ++j)
-                result += basicRoman[i];
-            num = num % basicInt[i];
-            
+        // Note: The Solution object is instantiated only once and is reused by each test case.    
+        static const string roman_base[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        static const int int_base[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        string ret;
+        for (int i = sizeof(int_base) / sizeof(int_base[0]) - 1; i >= 0 && num; --i) {
+            int len = num / int_base[i];
+            for (int j = 0; j < len; ++j) ret += roman_base[i];
+            num = num % int_base[i];
         }
-        
-        return result;
+        return ret;
     }
 };
--- 013RomanToInt.h
+++ 013RomanToInt.h
@@ -1,83 +1,86 @@
-class Solution {
+#include <unordered_map>
+using namespace std;
+
+class Solution01 {
 public:
     int romanToInt(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int result = 0;
-        for (size_t i = 0; i < s.length(); )
-        {
-            switch (s[i])
-            {
-                case 'M':
-                    result += 1000;
-                    ++i;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // static const string roman_base[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        // static const int int_base[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        int ret = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            switch (s[i]) {
+                case 'M' :
+                    ret += 1000;
                     break;
-                case 'D':
-                    result += 500;
-                    ++i;
+                case 'D' :
+                    ret += 500;
                     break;
-                case 'C':
-                    switch (s[i+1])
-                    {
-                        case 'M':
-                            result += 900;
-                            i += 2;
-                            break;
-                        case 'D':
-                            result += 400;
-                            i += 2;
-                            break;
-                        default:
-                            result += 100;
-                            ++i;
-                            break;
+                case 'C' :
+                    ++i;
+                    if (i < s.length() && 'M' == s[i]) {
+                        ret += 900;
+                    } else if (i < s.length() && 'D' == s[i]) {
+                        ret += 400;
+                    } else {
+                        ret += 100;
+                        --i;
                     }
                     break;
-                case 'L':
-                    result += 50;
-                    ++i;
+                case 'L' :
+                    ret += 50;
                     break;
-                case 'X':
-                    switch (s[i+1])
-                    {
-                        case 'C':
-                            result += 90;
-                            i += 2;
-                            break;
-                        case 'L':
-                            result += 40;
-                            i += 2;
-                            break;
-                        default:
-                            result += 10;
-                            ++i;
-                            break;
+                case 'X' :
+                    ++i;
+                    if (i < s.length() && 'C' == s[i]) {
+                        ret += 90;
+                    } else if (i < s.length() && 'L' == s[i]) {
+                        ret += 40;
+                    } else {
+                        ret += 10;
+                        --i;
                     }
                     break;
-                case 'V':
-                    result += 5;
-                    ++i;
+                case 'V' :
+                    ret += 5;
                     break;
-                case 'I':
-                    switch (s[i+1])
-                    {
-                        case 'X':
-                            result += 9;
-                            i += 2;
-                            break;
-                        case 'V':
-                            result += 4;
-                            i += 2;
-                            break;
-                        default:
-                            result += 1;
-                            ++i;
-                            break;
+                case 'I' :
+                    ++i;
+                    if (i < s.length() && 'X' == s[i]) {
+                        ret += 9;
+                    } else if (i < s.length() && 'V' == s[i]) {
+                        ret += 4;
+                    } else {
+                        ret += 1;
+                        --i;
                     }
                     break;
             }
         }
-        
-        return result;
+        return ret;
+    }
+};
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (s.empty()) return 0;
+        unordered_map<char, int> roman_decimal_map;
+        roman_decimal_map['M'] = 1000;
+        roman_decimal_map['D'] = 500;
+        roman_decimal_map['C'] = 100;
+        roman_decimal_map['L'] = 50;
+        roman_decimal_map['X'] = 10;
+        roman_decimal_map['V'] = 5;
+        roman_decimal_map['I'] = 1;
+        int ret = roman_decimal_map[s[s.length()-1]];
+        for (string::iterator i = s.begin(); i+1 != s.end(); ++i) {
+            if (roman_decimal_map[*i] < roman_decimal_map[*(i+1)])
+                ret -= roman_decimal_map[*i];
+            else
+                ret += roman_decimal_map[*i];
+        }
+        return ret;
     }
 };
