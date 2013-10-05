772edc58aa25ba0ad4559723cfa08b58aa179d32
Min WU <wumalbert@gmail.com>
2013-10-05 14:05:05 +0800

    code style optimization

--- 069Sqrt.h
+++ 069Sqrt.h
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int sqrt(int x) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-
-        int left = 0;
-        int right = x;
-
-        while (left <= right)
-        {
-            int mid = left + (right - left) / 2;
-            double t = (double)mid * mid;
-            if (t == x) return mid;
-            if (t < x) left = mid+1;
-            else if (t > x) right = mid-1;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (x < 0) return -1;
+        int left = 0, right = x;
+        while (left <= right) {
+            int middle = left + ((right-left) >> 1);
+            long long t = static_cast<long long>(middle) * middle;
+            if (t == x) return middle;
+            else if (t < x) left = middle+1;
+            else right = middle-1;
         }
-
         return right;
     }
 };
