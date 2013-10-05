bc244082c7ed59eac701666e7c009f433686e51c
Min WU <wumalbert@gmail.com>
2013-10-05 13:37:48 +0800

    code style optimization

--- 065AddBinary.h
+++ 065AddBinary.h
@@ -1,30 +1,23 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (a.empty()) return b;
-        if (b.empty()) return a;
-        
-        int len1 = a.length();
-        int len2 = b.length();
-        int len = max(len1, len2) + 1;
-        
-        string ret(len, '0');
-        int i = len1-1;
-        int j = len2-1;
-        int k = len-1;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        string ret("");
         int carry = 0;
-        while (k >= 0)
-        {
-            int t1 = i >= 0 ? a[i--]-'0' : 0;
-            int t2 = j >= 0 ? b[j--]-'0' : 0;
-            t1 = t1 + t2 + carry;
-            ret[k--] += t1 % 2;
-            carry = t1 / 2;
+        string::iterator ita = a.end()-1, itb = b.end()-1;
+        while (ita >= a.begin() || itb >= b.begin() || carry) {
+            int t = carry;
+            if (ita >= a.begin()) {
+                t += *ita - '0';
+                --ita;
+            }
+            if (itb >= b.begin()) {
+                t += *itb - '0';
+                --itb;
+            }
+            ret = static_cast<char>('0' + (t % 2)) + ret;
+            carry = t / 2;
         }
-        
-        for (k = 0; k < len-1 && '0' == ret[k]; ++k);
-        return ret.substr(k, len-k);
+        return ret;
     }
 };
