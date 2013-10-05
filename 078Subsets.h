f881336790f8f3a9288b17203de0e722a86df2d1
Min WU <wumalbert@gmail.com>
2013-10-05 15:40:37 +0800

    iteration solution

--- 078Subsets.h
+++ 078Subsets.h
@@ -1,36 +1,56 @@
-class Solution {
-private:
-    void subsetsRecursive(int k, vector<int>& S, vector<int> sub, vector<vector<int> >& ans)
-    {
-        if (S.size() == k)
-        {
-            ans.push_back(sub);
-        }
-        else
-        {
-            // not include S[k]  into the subset
-            subsetsRecursive(k+1, S, sub, ans);
-            
-            // include S[k] into the subset
-            sub.push_back(S[k]);
-            
-            subsetsRecursive(k+1, S, sub, ans);
-        }
-    }
-    
+class Solution02 {
 public:
     vector<vector<int> > subsets(vector<int> &S) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         sort(S.begin(), S.end());
-        
-        vector<int> sub;
         vector<vector<int> > ret;
-        
-        subsetsRecursive(0, S, sub, ret);
-        
+        vector<int> current;
+        subsetsRecursive(S, 0, current, ret);
         return ret;
-        
+    }
+private:
+    void subsetsRecursive(vector<int> &S, int index, vector<int> &current, vector<vector<int> > &ans) {
+        if (S.size() == index) {
+            ans.push_back(current);
+            return;
+        }
+        subsetsRecursive(S, index+1, current, ans);
+        current.push_back(S[index]);
+        subsetsRecursive(S, index+1, current, ans);
+        current.pop_back();
+    }
+};
+
+class Solution01 {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        sort(S.begin(), S.end());
+        vector<vector<int> > ret;
+        int n = S.size();
+        for (int i = (1 << n) - 1; i >= 0; --i) {
+            vector<int> current;
+            for (int j = 0; j < n; ++j) {
+                if (i & (1<<j)) current.push_back(S[j]);
+            }
+            ret.push_back(current);
+        }
+        return ret;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        sort(S.begin(), S.end());
+        vector<vector<int> > ret(1, vector<int>());
+        for (vector<int>::iterator i = S.begin(); i != S.end(); ++i) {
+            for (int j = ret.size()-1; j >= 0; --j) {
+                ret.push_back(ret[j]);
+                ret.back().push_back(*i);
+            }
+        }
+        return ret;
     }
 };
