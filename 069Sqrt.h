c0bf111d9ccf5f13ba7eaab89fa53d9b116834c2
Min WU <wumalbert@gmail.com>
2013-10-05 14:15:55 +0800

    fix a bug of sqrt(0)

--- 069Sqrt.h
+++ 069Sqrt.h
@@ -3,12 +3,12 @@ public:
     int sqrt(int x) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         if (x < 0) return -1;
-        int left = 0, right = x;
+        if (0 == x) return 0;
+        int left = 1, right = x;
         while (left <= right) {
             int middle = left + ((right-left) >> 1);
-            long long t = static_cast<long long>(middle) * middle;
-            if (t == x) return middle;
-            else if (t < x) left = middle+1;
+            if (middle == x/middle) return middle;
+            else if (middle < x/middle) left = middle+1;
             else right = middle-1;
         }
         return right;
