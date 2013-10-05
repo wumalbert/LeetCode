589616b671489f2f0a657e1302876c4312dfd5ef
Min WU <wumalbert@gmail.com>
2013-10-05 12:12:58 +0800

    code style optimization

--- 048Anagrams.h
+++ 048Anagrams.h
@@ -1,26 +1,19 @@
 class Solution {
 public:
     vector<string> anagrams(vector<string> &strs) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        map<string, vector<string> > groups;
-        for (vector<string>::iterator it = strs.begin(); it != strs.end(); ++it)
-        {
-            string key = *it;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        unordered_map<string, vector<string> > anagrams_map;
+        for (vector<string>::iterator it = strs.begin(); it != strs.end(); ++it) {
+            string key(*it);
             sort(key.begin(), key.end());
-            groups[key].push_back(*it);
+            anagrams_map[key].push_back(*it);
         }
-        
         vector<string> ret;
-        for(map<string, vector<string> >::iterator it = groups.begin(); it != groups.end(); ++it)
-        {
-            if (it->second.size() > 1)
-            {
-                for (vector<string>::iterator i = it->second.begin(); i != it->second.end(); ++i)
-                    ret.push_back(*i);
+        for (unordered_map<string, vector<string> >::iterator it = anagrams_map.begin(); it != anagrams_map.end(); ++it) {
+            if (it->second.size() > 1) {
+                ret.insert(ret.end(), it->second.begin(), it->second.end());
             }
         }
-        
         return ret;
     }
 };
