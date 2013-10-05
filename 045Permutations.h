6e71bfe1cedb4b012178df773bf4a0c08d23ac8c
Min WU <wumalbert@gmail.com>
2013-10-05 11:43:24 +0800

    recursive solution

--- 045Permutations.h
+++ 045Permutations.h
@@ -55,3 +55,53 @@ public:
         return ret;
     }
 };
+// recursive DFS
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<bool> choosed(num.size(), false);
+        vector<int> current;
+        vector<vector<int> > ret;
+        permute(num, choosed, 0, current, ret);
+        return ret;
+    }
+private:
+    void permute(const vector<int> &num, vector<bool> &choosed, int index, vector<int> &current, vector<vector<int> > &ans) {
+        if (num.size() == index) {
+            ans.push_back(current);
+            return;
+        }
+        for (int i = 0; i < num.size(); ++i) {
+            if (!choosed[i]) {
+                choosed[i] = true;
+                current.push_back(num[i]);
+                permute(num, choosed, index+1, current, ans);
+                current.pop_back();
+                choosed[i] = false;
+            }
+        }
+    }
+};
+// more elegant recursive solution
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<vector<int> > ret;
+        dfs(num, 0, ret);
+        return ret;
+    }
+private:
+    void dfs(vector<int> &num, int position, vector<vector<int> > &ans) {
+        if (num.size() == position) {
+            ans.push_back(num);
+            return;
+        }
+        for (int i = position; i < num.size(); ++i) {
+            swap(num[i], num[position]);
+            dfs(num, position+1, ans);
+            swap(num[i], num[position]);
+        }
+    }
+};
