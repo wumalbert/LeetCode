ea9970e16e52775e9196c42ada9f06f1ba08ac3e
wumalbert <wumalbert@gmail.com>
2013-10-05 19:30:25 +0800

    memory optimization

--- 079WordSearch.h
+++ 079WordSearch.h
@@ -42,3 +42,34 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool exist(vector<vector<char> > &board, string word) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (board.empty() || board[0].empty() || word.empty()) return false;
+        int m = board.size(), n = board[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (dfs(board, word, i, j, 0)) return true;
+            }
+        }
+    }
+private:
+    bool dfs(vector<vector<char> > &board, const string &word, int row, int col, int index) {
+        static const int dx[] = {1, 0, -1,  0};  // the column increment
+        static const int dy[] = {0, 1,  0, -1};  // the row increment
+        if (board[row][col] != word[index]) return false;
+        if (word.length()-1 == index) return true;
+        board[row][col] = '#';
+        for (int i = 0; i < 4; ++i) {
+            int x = row + dx[i], y = col + dy[i];
+            if (inboard(board.size(), board[0].size(), x, y) && dfs(board, word, x, y, index+1)) return true;
+        }
+        board[row][col] = word[index];
+        return false;
+    }
+    bool inboard(int m, int n, int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n;
+    }
+};
