c018e3e4375283a42e09cf3d556bb2d23b9c44b4
wumalbert <wumalbert@gmail.com>
2013-10-07 15:44:39 +0800

    iteration solution

--- 018LetterCombinations.h
+++ 018LetterCombinations.h
@@ -1,49 +1,46 @@
 class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<string> ret;
+        string str;
+        letterCombinations(ret, str, digits, 0);
+        return ret;
+    }
 private:
-    void letterCombinations(vector<string>& results, string& str, size_t& nZero, 
-        const string& digits, size_t i)
-    {
-        static const string digitMap[] = {" ", "", "abc", "def", "ghi", 
+    void letterCombinations(vector<string>& results, string& str, const string& digits, int index) {
+        static const string digit_alpha_map[] = {" ", "", "abc", "def", "ghi", 
             "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-        if (i == digits.size())
-        {
+        if (index == digits.size()) {
             results.push_back(str);
             return;
         }
-
-
-        if (digits[i] == '0')
-        {
-            ++nZero;
-
-            letterCombinations(results, str, nZero, digits, i+1);
-
-            str = str.substr(0, i-nZero);
-        }
-        else
-        {
-            for (size_t j = 0; j < digitMap[digits[i] - '0'].length(); ++j)
-            {
-                str += digitMap[digits[i] - '0'][j];
-
-                letterCombinations(results, str, nZero, digits, i+1);
-
-                str = str.substr(0, i-nZero);
-            }
+        int number = digits[index] - '0';
+        for (int j = 0; j < digit_alpha_map[number].length(); ++j) {
+            str.append(1, digit_alpha_map[number][j]);
+            letterCombinations(results, str, digits, index+1);
+            str.resize(str.size()-1);
         }
     }
+};
+class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-
-        vector<string> ret;
-        string str;
-        size_t nZero = 0;
-
-        letterCombinations(ret, str, nZero, digits, 0);
-
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<string> ret(1, "");
+        static const string digit_alpha_map[] = {" ", "", "abc", "def", "ghi", 
+            "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        for (int i = 0; i < digits.length(); ++i) {
+            if ('0' != digits[i]) {
+                for (int j = ret.size()-1; j >= 0; --j) {
+                    for (int k = digit_alpha_map[digits[i]-'0'].length() - 1; k > 0; --k) {
+                        ret.push_back(ret[j]);
+                        ret.back().append(1, digit_alpha_map[digits[i]-'0'][k]);
+                    }
+                    ret[j].append(1, digit_alpha_map[digits[i]-'0'][0]);
+                }
+            }
+        }
         return ret;
     }
 };
