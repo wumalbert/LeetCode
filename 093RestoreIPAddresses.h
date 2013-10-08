7215bd796d3007615f578c23de580ba1d9ebc1af
Min WU <wumalbert@gmail.com>
2013-10-08 20:35:53 +0800

    code style optimization

--- 093RestoreIPAddresses.h
+++ 093RestoreIPAddresses.h
@@ -1,45 +1,33 @@
 class Solution {
-private:
-    void restoreIpAddresses(string& s, int i, int j, string ip, vector<string>& ans)
-    {
-        if (4 == j && s.length() == i)
-        {
-            ans.push_back(ip.substr(0, ip.size()-1));
-        }
-        else if (4 > j && s.length() > i)
-        {            
-            if ('0' == s[i])
-            {
-                string t = ip + s.substr(i, 1) + ".";
-                restoreIpAddresses(s, i+1, j+1, t, ans);
-            }
-            else
-            {
-                int k;
-                for (k = 1; k < 3; ++k)
-                {
-                    string t = ip + s.substr(i, k) + ".";
-                    restoreIpAddresses(s, i+k, j+1, t, ans);
-                }
-                
-                int v = atoi(s.substr(i, k).c_str());
-                if (v < 256)
-                {
-                    string t = ip + s.substr(i, k) + ".";
-                    restoreIpAddresses(s, i+k, j+1, t, ans);
-                }
-            }
-        }
-    }
 public:
     vector<string> restoreIpAddresses(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         string ip;
         vector<string> ret;
-        
-        restoreIpAddresses(s, 0, 0, ip, ret);
-        
+        dfs(s, 0, 0, ip, ret);
         return ret;
+    }
+private:
+    void dfs(string &s, int str_index, int ip_index, string &ip, vector<string> &ans) {
+        if (4 == ip_index && str_index == s.size()) {
+            ans.push_back(ip.substr(0, ip.size()-1));
+            return;
+        }
+        if (ip_index >= 4 || str_index >= s.size()) return;
+        if ('0' == s[str_index]) {
+            ip.append("0.");
+            dfs(s, str_index+1, ip_index+1, ip, ans);
+            ip.resize(ip.size()-2);
+        } else {
+            int num = 0;
+            for (int i = 0; i < 3 && str_index+i < s.size(); ++i) {
+                num = num * 10 + s[str_index+i] - '0';
+                if (num < 256) {
+                    ip = ip + s.substr(str_index, i+1) + ".";
+                    dfs(s, str_index+i+1, ip_index+1, ip, ans);
+                    ip.resize(ip.size()-i-2);
+                }
+            }
+        }
     }
 };
