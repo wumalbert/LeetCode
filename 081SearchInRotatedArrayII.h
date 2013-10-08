32a89289d792554a6f36be89bd95c56dcd64173e
Min WU <wumalbert@gmail.com>
2013-10-08 19:30:11 +0800

    code style optimization

--- 081SearchInRotatedArrayII.h
+++ 081SearchInRotatedArrayII.h
@@ -1,40 +1,19 @@
 class Solution {
 public:
     bool search(int A[], int n, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int left = 0;
-        int right = n-1;
-        
-        while (left <= right)
-        {
-            int middle = left + (right-left)/2;
-            
-            if (target == A[middle]) return true;
-            
-            if (A[left] < A[middle]) // the left part are sorted
-            {
-                if (target >= A[left] && target < A[middle])
-                    right = middle-1;
-                else
-                    left = middle+1;
-            }
-            else if (A[left] > A[middle]) // the right part are sorted
-            {
-                if (target < A[middle] || target >= A[left])
-                    right = middle-1;
-                else
-                    left = middle+1;
-                
-                /*
-                if (target > A[middle] && target <= A[right])
-                    left = middle+1;
-                else
-                    right = middle-1; */
-            }
-            else
-            {
-                for(++left; left < right && A[left] == A[left-1]; ++left);
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int left = 0, right = n-1;
+        while (left <= right) {
+            int middle = left + (right-left >> 1);
+            if (A[middle] == target) return true;
+            if (A[middle] > A[left]) {
+                if (target < A[middle] && target >= A[left]) right = middle - 1;
+                else left = middle + 1;
+            } else if (A[middle] < A[left]) {
+                if (target > A[middle] && target <= A[right]) left = middle + 1;
+                else right = middle - 1;
+            } else {
+                ++left;
             }
         }
         return false;
