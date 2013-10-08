522773f0f2d12308c982747c2fa202a2fc3d1548
Min WU <wumalbert@gmail.com>
2013-10-08 16:59:47 +0800

    code style optimization

--- 032SearchInRotatedArray.h
+++ 032SearchInRotatedArray.h
@@ -1,30 +1,17 @@
 class Solution {
 public:
     int search(int A[], int n, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int left = 0;
-        int right = n-1;
-
-        while (left <= right)
-        {
-            int middle = left + (right-left)/2;
-
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int left = 0, right = n-1;
+        while (left <= right) {
+            int middle = left + (right-left >> 1);
             if (A[middle] == target) return middle;
-
-            if (A[left] <= A[middle]) // the left part is sorted
-            {
-                if (target >= A[left] && target < A[middle])
-                    right = middle - 1;
-                else
-                    left = middle + 1;
-            }
-            else // the right part is sorted
-            {
-                if (target > A[middle] && target <= A[right])
-                    left = middle + 1;
-                else
-                    right = middle - 1;
+            if (A[middle] >= A[left]) { // the left part is sorted
+                if (target < A[middle] && target >= A[left]) right = middle - 1;
+                else left = middle + 1;
+            } else {
+                if (target > A[middle] && target <= A[right]) left = middle + 1;
+                else right = middle - 1;
             }
         }
         return -1;
