0c543a477aa1e37bcd4ecb91c15d5c707d165e96
Min WU <wumalbert@gmail.com>
2013-10-08 15:35:36 +0800

    code style optimization

--- 062UniquePaths2.h
+++ 062UniquePaths2.h
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (obstacleGrid.empty()) return 0;
-        int m = obstacleGrid.size();
-        if (obstacleGrid.empty()) return 0;
-        int n = obstacleGrid[0].size();
-        
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (obstacleGrid.empty() || obstacleGrid[0].empty()) return 0;
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
         vector<int> paths(n, 0);
         paths[0] = 1;
-            
-        for (int i = 0; i < m; ++i)
-        {
-            if (1 == obstacleGrid[i][0])
-                paths[0] = 0;
-            for (int j = 1; j < n; ++j)
-            {
+        for (int i = 0; i < m; ++i) {
+            if (1 == obstacleGrid[i][0]) paths[0] = 0;
+            for (int j = 1; j < n; ++j) {
                 if (1 == obstacleGrid[i][j]) paths[j] = 0;
                 else paths[j] = paths[j-1] + paths[j];
             }
         }
-        
         return paths[n-1];
     }
 };
