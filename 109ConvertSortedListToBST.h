d6b9b3f8404bf7f9f3ceb024bb6f3de48182c9f5
Min WU <wumalbert@gmail.com>
2013-10-09 01:11:59 +0800

    time complexity improved to O(n)

--- 109ConvertSortedListToBST.h
+++ 109ConvertSortedListToBST.h
@@ -16,31 +16,48 @@
  * };
  */
 class Solution {
+public:
+    TreeNode *sortedListToBST(ListNode *head) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (NULL == head) return NULL;
+        return genBST(head, NULL);
+    }
 private:
-    TreeNode *sortedListToBST(const ListNode *begin, const ListNode *end)
-    {
+    // generate BST from [begin, end)
+    TreeNode *genBST(ListNode *begin, ListNode *end) {
         if (begin == end) return NULL;
-        
-        const ListNode *slow = begin, *fast = begin;
-        while (fast != end)
-        {
-            if (fast->next == end) break;
+        ListNode *slow = begin, *fast = begin;
+        while (fast != end && fast->next != end) {
             fast = fast->next->next;
             slow = slow->next;
         }
-        
         TreeNode *root = new TreeNode(slow->val);
-        root->left = sortedListToBST(begin, slow);
-        root->right = sortedListToBST(slow->next, end);
-        
+        root->left = genBST(begin, slow);
+        root->right = genBST(slow->next, end);
         return root;
     }
-    
+};
+class Solution {
 public:
     TreeNode *sortedListToBST(ListNode *head) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (!head) return NULL;
-        return sortedListToBST(head, NULL);
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (NULL == head) return NULL;
+        int begin = 0, end = 0;
+        ListNode *list = head;
+        for (; list; list = list->next) ++end;
+        list = head;
+        return genBST(list, begin, end);
+    }
+private:
+    // generate BST from [begin, end]
+    TreeNode *genBST(ListNode *&list, int begin, int end) {
+        if (begin >= end) return NULL;
+        int middle = begin + (end-begin >> 1);
+        TreeNode *left = genBST(list, begin, middle);
+        TreeNode *root = new TreeNode(list->val);
+        root->left = left;
+        list = list->next;
+        root->right = genBST(list, middle+1, end);
+        return root;
     }
 };
