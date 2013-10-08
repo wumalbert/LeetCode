80defe39fad02e0d349ad0949fb6ecf7745f63d1
Min WU <wumalbert@gmail.com>
2013-10-08 16:40:26 +0800

    code optimization

--- 074Search2DMatrix.h
+++ 074Search2DMatrix.h
@@ -1,37 +1,24 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int> > &matrix, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        if (matrix.empty()) return false;
-        int m = matrix.size();
-        if (matrix[0].empty()) return false;
-        int n = matrix[0].size();
-        
-        // 1. locate the row
-        int left = 0, right = m-1;
-        
-        while (left <= right)
-        {
-            int mid = left + (right-left)/2;
-            if (matrix[mid][0] == target) return true;
-            if (matrix[mid][0] < target) left = mid + 1;
-            else if (matrix[mid][0] > target) right = mid - 1;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (matrix.empty() || matrix[0].empty()) return false;
+        int left = 0, right = matrix.size()-1;
+        while (left <= right) {
+            int middle = left + (right - left >> 1);
+            if (matrix[middle][0] == target) return true;
+            else if (matrix[middle][0] < target) left = middle + 1;
+            else right = middle - 1;
         }
-        
-        // (right, left)
         if (right < 0) return false;
         int row = right;
-        
         left = 0;
-        right = n-1;
-        while (left <= right)
-        {
-            int mid = left + (right-left)/2;
-            if (matrix[row][mid] == target) return true;
-            if (matrix[row][mid] < target) left = mid + 1;
-            else if (matrix[row][mid] > target) right = mid - 1;
+        right = matrix[row].size()-1;
+        while (left <= right) {
+            int middle = left + (right-left >> 1);
+            if (matrix[row][middle] == target) return true;
+            else if (matrix[row][middle] < target) left = middle + 1;
+            else right = middle - 1;
         }
         return false;
     }
