223dea56fe0975a92c000790c2c8e02dfa10365b
Min WU <wumalbert@gmail.com>
2013-10-08 15:27:06 +0800

    code style optimization

--- 061UniquePaths.h
+++ 061UniquePaths.h
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<int> paths(n, 1);
-        
-        for (int i = 1; i < m; ++i)
-        {
-            for (int j = 1; j < n; ++j)
-            {
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
                 paths[j] = paths[j-1] + paths[j];
             }
         }
-        
         return paths[n-1];
     }
 };
