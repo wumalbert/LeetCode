9800c0697bc8b4e98f81beed8b6e915a3d706903
Min WU <wumalbert@gmail.com>
2013-10-08 20:56:16 +0800

    dfs, iteration with stack and morris traversal

--- 094BinaryTreeInorderTraversal.h
+++ 094BinaryTreeInorderTraversal.h
@@ -9,64 +9,64 @@
  */
 class Solution {
 public:
-    vector<int> inorderTraversal01(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+    vector<int> inorderTraversal(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<int> ret;
-        
-        stack<TreeNode*> inorderStack;
-        
-        TreeNode *node = root;
-        while (node)
-        {
-            inorderStack.push(node);
-            node = node->left;
-        }
-        
-        while (!inorderStack.empty())
-        {
-            TreeNode *node = inorderStack.top();
-            inorderStack.pop();
-            ret.push_back(node->val);
-            
-            if (node->right)
-            {
-                node = node->right;
-                while (node)
-                {
-                    inorderStack.push(node);
-                    node = node->left;
-                }
-            }
-        }
-        
+        dfs(root, ret);
         return ret;
     }
-
+private:
+    void dfs(TreeNode *root, vector<int> &ans) {
+        if (!root) return;
+        dfs(root->left, ans);
+        ans.push_back(root->val);
+        dfs(root->right, ans);
+    }
+};
+class Solution {
+public:
     vector<int> inorderTraversal(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<int> ret;
-
+        stack<TreeNode*> inorder_stack;
         TreeNode *current = root;
-        stack<TreeNode*> inorderStack;
-
-        while (!inorderStack.empty() || current)
-        {
-            if (current)
-            {
-                inorderStack.push(current);
+        while (!inorder_stack.empty() || current) {
+            if (current) {
+                inorder_stack.push(current);
                 current = current->left;
-            }
-            else
-            {
-                current = inorderStack.top();
-                inorderStack.pop();
+            } else {
+                current = inorder_stack.top();
+                inorder_stack.pop();
                 ret.push_back(current->val);
                 current = current->right;
             }
         }
-
+        return ret;
+    }
+};
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<int> ret;
+        TreeNode *current = root;
+        while (current) {
+            if (current->left) {
+                TreeNode *node = current->left;
+                while (node->right && node->right != current) node = node->right;
+                if (!node->right) {
+                    node->right = current;
+                    current = current->left;
+                } else {
+                    ret.push_back(current->val);
+                    node->right = NULL;
+                    current = current->right;
+                }
+            } else {
+                ret.push_back(current->val);
+                current = current->right;
+            }
+        }
         return ret;
     }
 };
