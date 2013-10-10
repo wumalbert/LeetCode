9b6ba90ae323ccb5ef04b8fff557796dbdf3909f
Min WU <wumalbert@gmail.com>
2013-10-10 23:49:13 +0800

    code opitimization

--- 005LongestPalindromic.h
+++ 005LongestPalindromic.h
@@ -1,63 +1,38 @@
 class Solution {
-    
-private:
-    string preprocess(string& s)
-    {
-        string ns;
-        ns += "^#";
-        for(size_t i = 0; i < s.length(); ++i)
-        {
-            ns += s.substr(i, 1);
-            ns += "#";
-        }
-        ns += "$";
-        
-        return ns;
-    }
-    
 public:
     string longestPalindrome(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        if (s.empty()) return s;
-        
-        int n = 2 * s.length() + 3;
-        string ns = preprocess(s);
-        
-        int* radius = new int[n];
-        int maxCenter, maxRadius;
-        maxCenter = maxRadius = 0;
-        for (int i = 1; i < n-1; ++i)
-        {
-            if (maxRadius > i)
-            {
-                int j = 2*maxCenter - i;
-                radius[i] = (maxRadius - i) > radius[j] ? radius[j] : (maxRadius - i);
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (s.empty()) return "";
+        int len = 2 * s.length() + 3;
+        string str(len, '#');
+        str[len-1] = '$';
+        for (string::iterator i = s.begin(), j = str.begin()+2; i != s.end(); ++i, j += 2)
+            *j = *i;
+        int farthest = 0, center = 0;
+        vector<int> palindrome_length(len, 0);
+        for (int i = 1; i < len-1; ++i) {
+            if (i < farthest) {
+                int j = 2*center - i;
+                if (palindrome_length[j] + i <= farthest) {
+                    palindrome_length[i] = palindrome_length[j];
+                } else {
+                    palindrome_length[i] = max(farthest-i-1, 0);
+                }
             }
-            else radius[i] = 0;
-            
-            for (; ns[i - radius[i]- 1] == ns[i + radius[i] + 1]; ++radius[i]);
-            
-            if (i + radius[i] > maxRadius)
-            {
-                maxCenter = i;
-                maxRadius = i + radius[i];
+            while (str[i+palindrome_length[i]+1] == str[i-palindrome_length[i]-1])
+                ++palindrome_length[i];
+            if (i + palindrome_length[i] > farthest) {
+                farthest = i + palindrome_length[i];
+                center = i;
             }
         }
-        
-        maxCenter = maxRadius = 0;
-        for (int i = 1; i < n-1; ++i)
-        {
-            if (radius[i] > maxRadius)
-            {
-                maxRadius = radius[i];
-                maxCenter = i;
+        farthest = center = 0;
+        for (int i = 2; i < len-1; ++i) {
+            if (palindrome_length[i] > farthest) {
+                farthest = palindrome_length[i];
+                center = i;
             }
         }
-        
-        delete [] radius;
-        
-        return s.substr((maxCenter - maxRadius - 1)/2, maxRadius);
+        return s.substr((center-farthest-1)/2, farthest);
     }
 };
