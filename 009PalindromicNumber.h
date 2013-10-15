6cdf0b8f4d5dab85a650ef2e96125145f7c614e6
wumalbert <wumalbert@gmail.com>
2013-10-15 17:52:22 +0800

    memory optimization

--- 009PalindromicNumber.h
+++ 009PalindromicNumber.h
@@ -1,4 +1,4 @@
-class Solution {
+class Solution01 {
 public:
     bool isPalindrome(int x) {
         // Start typing your C/C++ solution below
@@ -23,5 +23,20 @@ public:
         if (r == old) return true;
 
         return false;
+    }
+};
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (x < 0) return false;
+        int high = 1;
+        for (int y = x/10; y / high != 0; high *= 10) {}
+        while (x / 10) {
+            if (x / high != x % 10) return false;
+            x = (x % high) / 10;
+            high /= 100;
+        }
+        return true;
     }
 };
