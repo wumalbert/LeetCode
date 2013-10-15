5504f2c167e9d2f1a231bdae7643001063358e27
wumalbert <wumalbert@gmail.com>
2013-10-15 16:14:24 +0800

    code style optimization

--- 116PopulatingNextRightPointers.h
+++ 116PopulatingNextRightPointers.h
@@ -6,77 +6,39 @@
  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  * };
  */
-class Solution01 {
 public:
     void connect(TreeLinkNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        // morris traversal
-        TreeLinkNode *current = root, *left = NULL, *right = NULL;
-        
-        while (current)
-        {
-            if (!current->left)
-            {
-                current = current->right;
-            }
-            else
-            {
-                left = current->left;
-                right = current->right;
-                left->next = right;
-                while (left->right && left->right != current)
-                {
-                    left = left->right;
-                    right = right->left;
-                    left->next = right;
-                }
-                
-                if (!left->right)
-                {
-                    left->right = current;
-                    current = current->left;
-                }
-                else
-                {
-                    left->right = NULL;
-                    current = current->right;
-                }
-            }
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!root || !root->left) return;
+        root->left->next = root->right;
+        if (root->next) {
+            root->right->next = root->next->left;
         }
+        connect(root->left);
+        connect(root->right);
     }
 };
-
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        TreeLinkNode *current = root;
-        while (current)
-        {
-            // find the first node of the next level
-            TreeLinkNode *next = current->left ? current->left : current->right;
-            
-            // traversal the nodes in the current level
-            TreeLinkNode *previous = NULL;
-            for (; current; current = current->next)
-            {
-                if (current->left)
-                {
-                    if (previous) previous->next = current->left;
-                    previous = current->left;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        TreeLinkNode *current_level = root, *next_level = new TreeLinkNode(0);
+        while (current_level) {
+            TreeLinkNode *tail = next_level;
+            while (current_level) {
+                if (current_level->left) {
+                    tail->next = current_level->left;
+                    tail = tail->next;
                 }
-                if (current->right)
-                {
-                    if (previous) previous->next = current->right;
-                    previous = current->right;
+                if (current_level->right) {
+                    tail->next = current_level->right;
+                    tail = tail->next;
                 }
+                current_level = current_level->next;
             }
-            
-            current = next;
+            current_level = next_level->next;
+            next_level->next = NULL;
         }
+        delete next_level;
     }
 };
