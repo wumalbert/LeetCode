6ebd7ca69dfaaa0a100a16986477822b703ae209
wumalbert <wumalbert@gmail.com>
2013-10-17 00:19:55 +0800

    code style optimization

--- 044JumpGame2.h
+++ 044JumpGame2.h
@@ -1,52 +1,38 @@
-#include <vector>
-
 using namespace std;
 class Solution {
 public:
-    int jump01(int A[], int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
+    int jump(int A[], int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         if (!A || 0 == n) return 0;
-        
         // dynamic programming  
         vector<int> step(n, numeric_limits<int>::max());
         step[0] = 0;
-        
-        for (int i = 0; i < n-1; ++i)
-        {
-            // for (int j = i > A[i] ? i-A[i] : 0; j <= A[i] && i+j < n; ++j)
-            for (int j = 1; j <= A[i] && i+j < n; ++j)
-            {
-                if (step[i] + 1 < step[i+j])
-                {
+        for (int i = 0; i < n-1; ++i) {
+            for (int j = 1; j <= A[i] && i+j < n; ++j) {
+                if (step[i] + 1 < step[i+j]) {
                     step[i+j] = step[i] + 1;
                     if(i+j == n-1) return step[n-1];
                 }
             }
         }
-        
         return step[n-1];
     }
-
+};
+class Solution {
+public:
     int jump(int A[], int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int ret = 0;
-        int last = 0;
-        int curr = 0;
-
-        for (int i = 0; i < n; ++i)
-        {
-            if (i > last)
-            {
-                last = curr;
-                ++ret;
-
-                if (last >= n-1) return ret;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!A || n < 1) return -1;
+        if (1 == n) return 0;
+        int previous_reach = 0, next_reach = 0, steps = 0;
+        for (int i = 0; i <= previous_reach; ++i) {
+            next_reach = max(i+A[i], next_reach);
+            if (next_reach >= n-1) return steps+1;
+            if (i == previous_reach) {
+                ++steps;
+                previous_reach = next_reach;
             }
-
-            curr = max(curr, i + A[i]);
         }
+        return -1;
     }
 };
