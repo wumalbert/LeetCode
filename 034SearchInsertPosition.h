b2594f1ad84212e6b8c4e093b57ab995b7dd97c7
wumalbert <wumalbert@gmail.com>
2013-10-16 16:09:21 +0800

    code style optimization

--- 034SearchInsertPosition.h
+++ 034SearchInsertPosition.h
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int searchInsert(int A[], int n, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int left = 0;
-        int right = n-1;
-        
-        while(left <= right)
-        {
-            int middle = left + (right - left) / 2;
-            if(target == A[middle]) return middle;
-            
-            if (target < A[middle]) right = middle-1;
-            else left = middle+1;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!A || 0 == n) return -1;
+        int left = 0, right = n-1;
+        while (left <= right) {
+            int middle = left + (right-left >> 1);
+            if (A[middle] == target) return middle;
+            else if (A[middle] < target) left = middle + 1;
+            else right = middle-1;
         }
-        
         return left;
     }
 };
