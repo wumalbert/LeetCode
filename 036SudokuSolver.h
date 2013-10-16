01982a7e41e862c8decb90830437306941316360
wumalbert <wumalbert@gmail.com>
2013-10-16 22:03:00 +0800

    space complexity optimization

--- 036SudokuSolver.h
+++ 036SudokuSolver.h
@@ -1,73 +1,43 @@
 class Solution {
-private:
-    static const int kSize = 9;
-
-    bool solveSudokuRecursive(vector<vector<char> > &board, int n, 
-        vector<vector<bool> >& rowFlags,
-        vector<vector<bool> >& columnFlags,
-        vector<vector<bool> >& blockFlags
-        ) {
-            int i = n / kSize;
-            int j = n % kSize;
-
-            if ('.' == board[i][j])
-            {
-                // find the candidate value
-                for (int k = 0; k < kSize; ++k)
-                {
-                    if(rowFlags[i][k] || columnFlags[j][k] || blockFlags[(i/3)*3 + j/3][k])
-                        continue;
-
-                    board[i][j] = k + '1';
-                    rowFlags[i][k] = true;
-                    columnFlags[j][k] = true;
-                    blockFlags[(i/3)*3 + j/3][k] = true;
-
-                    if (n == kSize*kSize-1) return true;
-
-                    if (solveSudokuRecursive(board, n+1, rowFlags, columnFlags, blockFlags))
-                        return true;
-
-                    board[i][j] = '.';
-                    rowFlags[i][k] = false;
-                    columnFlags[j][k] = false;
-                    blockFlags[(i/3)*3 + j/3][k] = false;
-                }
-            }
-            else
-            {
-                if (n == kSize*kSize-1) return true;
-
-                if (solveSudokuRecursive(board, n+1, rowFlags, columnFlags, blockFlags))
-                    return true;
-            }
-            return false;
-    }
-
 public:
     void solveSudoku(vector<vector<char> > &board) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        vector<vector<bool> > rowFlags(kSize, vector<bool>(kSize, false));
-        vector<vector<bool> > columnFlags(kSize, vector<bool>(kSize, false));
-        vector<vector<bool> > blockFlags(kSize, vector<bool>(kSize, false));
-
-        // set the initial flags
-        for (int i = 0; i < kSize; ++i)
-        {
-            for (int j = 0; j < kSize; ++j)
-            {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<int> masks(kWidth*3, 0);
+        for (int i = 0; i < kWidth; ++i) {
+            for (int j = 0; j < kWidth; ++j) {
                 if ('.' == board[i][j]) continue;
-                int t = board[i][j]-'1';
-                if (rowFlags[i][t]) return; //row invaild
-                rowFlags[i][t] = true;
-                if (columnFlags[j][t]) return; //column invaild
-                columnFlags[j][t] = true;
-                if (blockFlags[(i/3)*3 + j/3][t]) return; //block invaild
-                blockFlags[(i/3)*3 + j/3][t] = true;
+                int value = 1 << (board[i][j]-'1'), column = kWidth+j, block = kWidth*2 + i/3*3 + j/3;
+                if (masks[i]&value || masks[column]&value || masks[block]&value) return;
+                masks[i] = masks[i]|value;
+                masks[column] = masks[column]|value;
+                masks[block] = masks[block]|value;
             }
         }
-
-        solveSudokuRecursive(board, 0, rowFlags, columnFlags, blockFlags);
+        solve(board, masks, 0);
     }
+private:
+    bool solve(vector<vector<char> > &board, vector<int> &masks, int index) {
+        if (kSize == index) return true;
+        int row = index / kWidth, column = index % kWidth;
+        if ('.' == board[row][column]) {
+            int column_index = kWidth + column, block_index = kWidth*2 + row/3*3 + column/3;
+            for (int i = 0; i < kWidth; ++i) {
+                int value = 1 << i;
+                if (masks[row]&value || masks[column_index]&value || masks[block_index]&value) continue;
+                masks[row] = masks[row]|value;
+                masks[column_index] = masks[column_index]|value;
+                masks[block_index] = masks[block_index]|value;
+                board[row][column] = static_cast<char>('1'+i);
+                if (solve(board, masks, index+1)) return true;
+                masks[row] = masks[row]^value;
+                masks[column_index] = masks[column_index]^value;
+                masks[block_index] = masks[block_index]^value;
+                board[row][column] = '.';
+            }
+        } else if (solve(board, masks, index+1)) {
+            return true;
+        }
+        return false;
+    }
+    static const int kWidth = 9, kSize = 81;
 };
