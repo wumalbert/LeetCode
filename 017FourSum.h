297dacfb727237b442e885aa2a8debeb7c3780b0
wumalbert <wumalbert@gmail.com>
2013-10-16 10:31:35 +0800

    code style optimization

--- 017FourSum.h
+++ 017FourSum.h
@@ -1,48 +1,28 @@
 class Solution {
 public:
     vector<vector<int> > fourSum(vector<int> &num, int target) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<vector<int> > ret;
-
         if (num.size() < 4) return ret;
-
         sort(num.begin(), num.end());
-
-        size_t i = 0;
-        while (i < num.size()-3)
-        {   
-            size_t j = i + 1;
-            while (j < num.size() - 2)
-            {   
-                size_t left = j+1;
-                size_t right = num.size() - 1;
-
-                while (left < right)
-                {
-                    int t = num[i] + num[j] + num[left] + num[right];
-                    if (t == target)
-                    {
+        for (int i = 0; i < num.size()-3; ++i) {
+            if (i > 0 && num[i] == num[i-1]) continue;
+            for (int j = i+1; j < num.size()-2; ++j) {
+                if (j > i+1 && num[j] == num[j-1]) continue;
+                int left = j+1, right = num.size()-1;
+                while (left < right) {
+                    int sum = num[i] + num[j] + num[left] + num[right];
+                    if (sum == target) {
                         int v[4] = {num[i], num[j], num[left], num[right]};
                         ret.push_back(vector<int>(v, v+4));
-
-                        for (++left; num[left] == num[left-1] && left < right; ++left);
-                        for (--right; num[right] == num[right+1] && left < right; --right);
-                    }
-                    else if (t < target)
-                    {
+                    } else if (sum < target) {
                         ++left;
-                    }
-                    else
-                    {
+                    } else {
                         --right;
                     }
                 }
-                for (++j; num[j] == num[j-1]; ++j);
             }
-            for (++i; num[i] == num[i-1]; ++i);
         }
-
         return ret;
     }
 };
