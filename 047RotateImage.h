1890d34d372d8cbdeb84a8724d5042878e88d917
wumalbert <wumalbert@gmail.com>
2013-10-17 21:20:35 +0800

    code style optimization

--- 047RotateImage.h
+++ 047RotateImage.h
@@ -1,61 +1,41 @@
 class Solution {
-private:
-    void swap(int& v1, int& v2)
-    {
-        if (v1 == v2) return;
-
-        v1 = v1 ^ v2;
-        v2 = v1 ^ v2;
-        v1 = v1 ^ v2;
-    }
-
 public:
-    void rotate01(vector<vector<int> > &matrix) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-
-        // (i, j) is mapped to (H-1-j, i)
+    void rotate(vector<vector<int> > &matrix) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        // 1. transpose the matrix
         int width = matrix.size();
-
-        // 1. tranpose the image
-        for (int i = 0; i < width; ++i)
-        {
-            for (int j = i; j < width; ++j)
-            {
+        for (int i = 0; i < width; ++i) {
+            for (int j = i; j < width; ++j) {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-
-        // 2. swap every row
-        for (int i = 0; i < width; ++i)
-        {
-            int left = 0;
-            int right = width-1;
-            while (left < right)
-            {
+        for (int i = 0; i < width; ++i) {
+            for (int left = 0, right = width-1; left < right; ++left, --right) {
                 swap(matrix[i][left], matrix[i][right]);
-                ++left;
-                --right;
             }
         }
     }
-
+private:
+    void swap(int &a, int &b) {
+        if (a != b) {
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+        }
+    }
+};
+class Solution {
+public:
     void rotate(vector<vector<int> > &matrix) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-
-        // (i, j) is mapped to (H-1-j, i)
-        int w = matrix.size();
-
-        for (int i = 0; i < w/2; ++i)
-        {
-            for (int j = i; j < w-1-i; ++j)
-            {
-                int t = matrix[i][j];
-                matrix[i][j] = matrix[w-1-j][i];
-                matrix[w-1-j][i] = matrix[w-1-i][w-1-j];
-                matrix[w-1-i][w-1-j] = matrix[j][w-1-i];
-                matrix[j][w-1-i] = t;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int width = matrix.size();
+        for (int i = 0; i < width/2; ++i) {
+            for (int j = i; j < width-i-1; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[width-1-j][i];
+                matrix[width-1-j][i] = matrix[width-1-i][width-1-j];
+                matrix[width-1-i][width-1-j] = matrix[j][width-1-i];
+                matrix[j][width-1-i] = temp;
             }
         }
     }
