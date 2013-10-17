1e8589a48901cde314d55289dbbfe343113df8cd
wumalbert <wumalbert@gmail.com>
2013-10-17 17:33:05 +0800

    recursive solution

--- 046Permutations2.h
+++ 046Permutations2.h
@@ -1,57 +1,63 @@
-class Solution {    
-private:
-    void swap(int& v1, int& v2)
-    {
-        if (v1 == v2) return;
-
-        v1 = v1 ^ v2;
-        v2 = v1 ^ v2;
-        v1 = v1 ^ v2;
+class Solution {
+public:
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<vector<int> > ret;
+        sort(num.begin(), num.end());
+        ret.push_back(num);
+        while (next(num)) ret.push_back(num);
+        return ret;
     }
-
-    bool nextPermute(vector<int>& num)
-    {
-        // find the first descending element
-        int i = 0;
-        for (i = num.size()-2; i >= 0 && num[i] >= num[i+1]; --i);
-
-        if (i < 0) return false;
-
-        // find the first element larger than num[i]
-        int j = 0;
-        for (j = num.size() - 1; j > i && num[j] <= num[i]; --j);
-
-        swap(num[i], num[j]);
-
-        ++i;
-        j = num.size() - 1;
-        while (i < j)
-        {
-            swap(num[i], num[j]);
-            ++i;
-            --j;
-        }
+private:
+    bool next(vector<int> &num) {
+        int first_decrease = num.size()-2;
+        while (first_decrease >= 0 && num[first_decrease] >= num[first_decrease+1])
+            --first_decrease;
+        if (first_decrease < 0) return false;
+        int first_larger = num.size()-1;
+        while (first_larger > first_decrease && num[first_larger] <= num[first_decrease])
+            --first_larger;
+        swap(num.begin() + first_decrease, num.begin() + first_larger);
+        reverse(num.begin()+first_decrease+1, num.end());
         return true;
     }
-
+    void swap(vector<int>::iterator a, vector<int>::iterator b) {
+        *a = *a ^ *b;
+        *b = *a ^ *b;
+        *a = *a ^ *b;
+    }
+    void reverse(vector<int>::iterator begin, vector<int>::iterator end) {
+        if (end - begin < 2) return;
+        --end;
+        while (begin < end) swap(begin++, end--);
+    }
+};
+class Solution {
 public:
-    vector<vector<int> > permute(vector<int> &num) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<vector<int> > ret;
-
         if (num.empty()) return ret;
-
         sort(num.begin(), num.end());
-
-        ret.push_back(num);
-
-        while (nextPermute(num))
-        {
-            ret.push_back(num);
-        }
-
+        vector<int> permute;
+        vector<bool> visited(num.size(), false);
+        dfs(num, visited, permute, ret);
         return ret;
+    }
+private:
+    void dfs(vector<int> &num, vector<bool> &visited, vector<int> &permute, vector<vector<int> > &ans) {
+        if (permute.size() == num.size()) {
+            ans.push_back(permute);
+            return;
+        }
+        for (int i = 0; i < visited.size(); ++i) {
+            if (visited[i]) continue;
+            if (i > 0 && num[i] == num[i-1] && !visited[i-1]) continue;
+            visited[i] = true;
+            permute.push_back(num[i]);
+            dfs(num, visited, permute, ans);
+            permute.pop_back();
+            visited[i] = false;
+        }
     }
 };
