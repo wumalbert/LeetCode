5cf6a900fba0ffa17fc71b0b632546a63f8e415e
Min WU <wumalbert@gmail.com>
2013-10-18 00:00:20 +0800

    code style optimization

--- 067PlusOne.h
+++ 067PlusOne.h
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int> &digits) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         int len = digits.size();
         vector<int> ret(len+1, 0);
-        
         ret[len] = (digits[len-1] + 1) % 10;
         int carry = (digits[len-1] + 1) / 10;
-        for (int i = len-1; i > 0; --i)
-        {
+        for (int i = len-1; i > 0; --i) {
             int t = digits[i-1] + carry;
             ret[i] = t % 10;
             carry = t / 10;
         }
         ret[0] = carry;
-        
         if (0 == ret[0]) ret.erase(ret.begin());
-        
         return ret;
     }
 };
