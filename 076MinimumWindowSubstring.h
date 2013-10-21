37eb974d69179e9816b4d4b049f4d6309c8a8919
wumalbert <wumalbert@gmail.com>
2013-10-21 22:45:49 +0800

    memory optimization with hashmap

--- 076MinimumWindowSubstring.h
+++ 076MinimumWindowSubstring.h
@@ -1,45 +1,31 @@
 class Solution {
 public:
     string minWindow(string S, string T) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        static const int kChar = 256;
-        vector<int> need(kChar, 0);
-        
-        for (int i = 0; i < T.length(); ++i)
-            ++need[T[i]];
-    
-        vector<int> found(kChar, 0);
-        int minWidth = numeric_limits<int>::max();
-        int minStart = 0;
-        int count = 0;
-        for (int begin = 0, end = 0; end < S.length(); ++end)
-        {
-            char c = S[end];
-            if (0 == need[c]) continue;
-            
-            ++found[c];
-            
-            if (found[c] <= need[c]) ++count;
-            
-            if (count == T.length())
-            {
-                while (0 == need[S[begin]] || found[S[begin]] > need[S[begin]])
-                {
-                    if (found[S[begin]] > need[S[begin]]) --found[S[begin]];
-                    ++begin;
-                }
-                
-                int window = end - begin + 1;
-                if (window < minWidth)
-                {
-                    minWidth = window;
-                    minStart = begin;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        unordered_map<char, int> required, founded;
+        for (string::iterator i = T.begin(); i != T.end(); ++i) ++required[*i];
+        int min_window_start = -1, min_window_length = S.length()+1, count = 0;
+        for (string::iterator start = S.begin(), current = S.begin(); current != S.end(); ++current) {
+            if (required.find(*current) == required.end()) continue;
+            ++founded[*current];
+            if (founded[*current] <= required[*current]) {
+                ++count;
+                if (count == T.length()) {
+                    while (start < current) {
+                        if (required.find(*start) == required.end()) ++start;
+                        else if (founded[*start] > required[*start]) --founded[*start++];
+                        else break;
+                    }
+                    if (current - start + 1 < min_window_length) {
+                        min_window_start = start - S.begin();
+                        min_window_length = current - start + 1;
+                    }
+                    --founded[*start++];
+                    --count;
                 }
             }
         }
-        
-        if (numeric_limits<int>::max() == minWidth) return "";
-        return S.substr(minStart, minWidth);
+        if (min_window_start < 0) return "";
+        else return S.substr(min_window_start, min_window_length);
     }
 };
