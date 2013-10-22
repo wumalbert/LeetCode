71dd3263069be17b7ccc82ce3ab39b2d76c6055f
wumalbert <wumalbert@gmail.com>
2013-10-22 19:02:52 +0800

    different traverse methods

--- 101SymmetricTree.h
+++ 101SymmetricTree.h
@@ -7,53 +7,84 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution01 {
-private:
-    bool isSymmetric(TreeNode *left, TreeNode *right)
-    {
-        if (!left && !right) return true;
-        if (!left || !right) return false;
-        if (left->val != right->val) return false;
-        
-        return isSymmetric(left->left, right->right) && isSymmetric(left->right, right->left);
-    }
-    
-public:
-    bool isSymmetric(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (!root) return true;
-        
-        return isSymmetric(root->left, root->right);
-    }
-};
-
 class Solution {
 public:
     bool isSymmetric(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         if (!root) return true;
-
-        queue<TreeNode*> leftQueue;
-        queue<TreeNode*> rightQueue;
-
-        leftQueue.push(root->left);
-        rightQueue.push(root->right);
-
-        while (!leftQueue.empty() && !rightQueue.empty())
-        {
-            TreeNode* left = leftQueue.front();
-            TreeNode* right = rightQueue.front();
-            leftQueue.pop();
-            rightQueue.pop();
-
+        return dfs(root->left, root->right);
+    }
+private:
+    bool dfs(TreeNode *left, TreeNode *right) {
+        if (!left && !right) return true;
+        if (!left || !right || left->val != right->val) return false;
+        return dfs(left->left, right->right) && dfs(left->right, right->left);
+    }
+};
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!root) return true;
+        queue<TreeNode*> left_queue, right_queue;
+        left_queue.push(root->left);
+        right_queue.push(root->right);
+        while (!left_queue.empty() && !right_queue.empty()) {
+            TreeNode *left = left_queue.front(), *right = right_queue.front();
+            left_queue.pop();
+            right_queue.pop();
             if (!left && !right) continue;
             if (!left || !right || left->val != right->val) return false;
-            leftQueue.push(left->left);
-            leftQueue.push(left->right);
-            rightQueue.push(right->right);
-            rightQueue.push(right->left);
+            left_queue.push(left->left);
+            left_queue.push(left->right);
+            right_queue.push(right->right);
+            right_queue.push(right->left);
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!root) return true;
+        stack<TreeNode*> left_stack, right_stack;
+        left_stack.push(root->left);
+        right_stack.push(root->right);
+        while (!left_stack.empty() && !right_stack.empty()) {
+            TreeNode *left = left_stack.top(), *right = right_stack.top();
+            left_stack.pop();
+            right_stack.pop();
+            if (!left && !right) continue;
+            if (!left || !right || left->val != right->val) return false;
+            left_stack.push(left->left);
+            left_stack.push(left->right);
+            right_stack.push(right->right);
+            right_stack.push(right->left);
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!root) return true;
+        vector<TreeNode*> layer[2];
+        layer[0].push_back(root);
+        int current = 0;
+        while (!layer[current].empty()) {
+            layer[1-current].clear();
+            for (vector<TreeNode*>::iterator i = layer[current].begin(); i != layer[current].end(); ++i) {
+                if(!*i) continue;
+                layer[1-current].push_back((*i)->left);
+                layer[1-current].push_back((*i)->right);
+            }
+            for (vector<TreeNode*>::iterator i = layer[1-current].begin(), j = layer[1-current].end()-1; i < j; ++i, --j) {
+                if (!*i && !*j) continue;
+                if (!*i || !*j || (*i)->val != (*j)->val) return false;
+            }
+            current = 1-current;
         }
         return true;
     }
