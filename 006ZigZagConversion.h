f1b9605f8e921c43e85f8192189eb167ee5d75c1
wumalbert <wumalbert@gmail.com>
2013-10-23 22:15:38 +0800

    code optimization

--- 006ZigZagConversion.h
+++ 006ZigZagConversion.h
@@ -1,58 +1,38 @@
-class Solution {
-private:
-    string convert01(string s, int nRows)
-    {
-        string* dst = new string[nRows];
-        
-        int stepInterval = 2 * nRows - 2;
-        
-        int i = 0;
-        while (i < s.length())
-        {
-            int j = 0;
-            for (; j < nRows && i < s.length(); ++j)
-                dst[j] += s[i++];
-            for (; j < stepInterval && i < s.length(); ++j)
-                dst[stepInterval - j] += s[i++];
-        }
-        
-        string result;
-        for (int i = 0; i < nRows; ++i)
-            result += dst[i];
-        
-        delete [] dst;
-        
-        return result;
-    }
-    
-    string converst02(string s, int nRows)
-    {
-        if (1 == nRows) return s;
-        
-        int step = 2 * nRows - 2;
-        
-        string dst;
-        for(int i = 0; i < nRows; ++i)
-        {
-            int j = i;
-            while (j < s.length())
-            {
-                dst += s[j];
-                
-                if (j%step != 0 && 2*(nRows-i)-2 != 0 && j+2*(nRows-i)-2 < s.length())    
-                    dst += s[j+2*(nRows-i)-2];
-                    
-                j += step;
-            }
-        }
-        
-        return dst;
-    }
-    
+class Solution01 {
 public:
     string convert(string s, int nRows) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function    
-        return converst02(s, nRows);
+        // Note: The Solution object is instantiated only once and is reused by each test case.    
+        int len = s.length();
+        vector<string> rows(nRows, "");
+        int cycle_length = 2*nRows-2;
+        for (int i = 0; i < len; ) {
+            int j;
+            for (j = 0; j < nRows && i < len; ++j) {
+                rows[j] += s[i++];
+            }
+            for (j = cycle_length - j; j > 0 && i < len; --j) {
+                rows[j] += s[i++];
+            }
+        }
+        string ret;
+        for (int i = 0; i < nRows; ++i) ret += rows[i];
+        return ret;
+    }
+};
+class Solution {
+public:
+    string convert(string s, int nRows) {
+        // Note: The Solution object is instantiated only once and is reused by each test case. 
+        if (1 == nRows) return s;
+        int len = s.length(), step = 2*nRows-2;
+        string ret(len, ' ');
+        for (int i = 0, count = 0; i < nRows; ++i) {
+            for (int j = i; j < len; j += step) {
+                ret[count++] = s[j];
+                if (i != 0 && i != nRows-1 && j + 2*(nRows-i) - 2 < len)
+                    ret[count++] = s[j + 2*(nRows-i) - 2];
+            }
+        }
+        return ret;
     }
 };
