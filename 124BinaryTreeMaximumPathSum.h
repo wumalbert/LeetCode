e502ffbc15c9ff289f580c981158c1fe880270a9
wumalbert <wumalbert@gmail.com>
2013-10-23 14:34:03 +0800

    code optimization

--- 124BinaryTreeMaximumPathSum.h
+++ 124BinaryTreeMaximumPathSum.h
@@ -8,36 +8,51 @@
  * };
  */
 class Solution {
-private:
-    void maxPathSum(TreeNode *root, int &m1, int &m2)
-    {
-        if (!root->left && !root->right)
-        {
-            m1 = m2 = root->val;
-            return;
-        }
-        int l1, l2, r1, r2;
-        l1 = l2 = r1 = r2 = numeric_limits<int>::min();
-        if(root->left) maxPathSum(root->left, l1, l2);
-        if(root->right) maxPathSum(root->right, r1, r2);
-        
-        int t = max(l2, r2);
-        if (t <= 0) m2 = root->val;
-        else m2 = t + root->val;
-        
-        m1 = max(l1, r1);
-        if (root->left && root->right) t = max(t, l2+r2);
-        if (t <= 0) m1 = max(m1, root->val);
-        else m1 = max(m1, t + root->val);
-    }
 public:
     int maxPathSum(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (!root) return numeric_limits<int>::min();
-        
-        int m1, m2;
-        maxPathSum(root, m1, m2);
-        return m1;
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!root) return 0;
+        int path_sum;
+        return dfs(root, path_sum);
+    }
+private:
+    int dfs(TreeNode *root, int &max_path) {
+        if (!root->left && !root->right) {
+            max_path = root->val;
+            return max_path;
+        }
+        int left, right, left_max, right_max;
+        left_max = right_max = left = right = numeric_limits<int>::min();
+        if (root->left) left_max = dfs(root->left, left);
+        else left = 0; 
+        if (root->right) right_max = dfs(root->right, right);
+        else right = 0;
+        max_path = root->val;
+        if (left + root->val > max_path) max_path = left + root->val;
+        if (right + root->val > max_path) max_path = right + root->val;
+        int ret = max(left_max, right_max);
+        if (left + right + root->val > ret) ret = left + right + root->val;
+        if (max_path > ret) ret = max_path;
+        return ret;
+    }
+};
+class Solution {
+public:
+    int maxPathSum(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int path_sum;
+        return dfs(root, path_sum);
+    }
+private:
+    int dfs(TreeNode *root, int &max_path) {
+        if (!root) {
+            max_path = numeric_limits<int>::min();
+            return max_path;
+        }
+        int left, right;
+        int ret = max(dfs(root->left, left), dfs(root->right, right));
+        ret = max(ret, root->val + (left > 0 ? left : 0) + (right > 0 ? right : 0));
+        max_path = max(0, max(left, right)) + root->val;
+        return ret;
     }
 };
