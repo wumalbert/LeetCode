ac82e91a6462dc8598fbef192b5495b0cb4f0995
wumalbert <wumalbert@gmail.com>
2013-10-23 10:29:36 +0800

    memory optimization

--- 110BalancedBinaryTree.h
+++ 110BalancedBinaryTree.h
@@ -8,30 +8,45 @@
  * };
  */
 class Solution {
+public:
+    bool isBalanced(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int depth = 0;
+        return dfs(root, depth);
+    }
 private:
-    bool isBalanced(TreeNode *root, int &depth)
-    {
-        if (!root)
-        {
+    bool dfs(TreeNode *root, int &depth) {
+        if (!root) {
             depth = 0;
             return true;
         }
-        else
-        {
-            int leftDepth = 0, rightDepth = 0;
-            if (!isBalanced(root->left, leftDepth)) return false;
-            if (!isBalanced(root->right, rightDepth)) return false;
-            if (abs(leftDepth-rightDepth) > 1) return false;
-            
-            depth = max(leftDepth, rightDepth) + 1;
-            return true;
-        }
+        int left_depth = 0, right_depth = 0;
+        bool ret;
+        if (dfs(root->left, left_depth) && 
+            dfs(root->right, right_depth) && 
+            left_depth - right_depth <= 1 && 
+            left_depth - right_depth >= -1)
+                ret = true;
+        else ret = false;
+        depth = max(left_depth, right_depth) + 1;
+        return ret;
     }
+};
+class Solution {
 public:
     bool isBalanced(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        int depth = 0;
-        return isBalanced(root, depth);
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int depth = getDepth(root);
+        return depth >= 0;
+    }
+private:
+    int getDepth(TreeNode *root) {
+        if (!root) return 0;
+        int left_depth = getDepth(root->left);
+        if (left_depth < 0) return -1;
+        int right_depth = getDepth(root->right);
+        if (right_depth < 0) return -1;
+        if (left_depth - right_depth < -1 || left_depth - right_depth > 1) return -1;
+        return max(left_depth, right_depth) + 1;
     }
 };
