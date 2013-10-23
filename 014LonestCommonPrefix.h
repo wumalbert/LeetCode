534f3652b4ffb19b9a560c0e47324cd3abc45bfb
wumalbert <wumalbert@gmail.com>
2013-10-23 22:45:02 +0800

    time complexity optimization

--- 014LonestCommonPrefix.h
+++ 014LonestCommonPrefix.h
@@ -1,26 +1,43 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string> &strs) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         if (strs.empty()) return "";
-        if (strs.size() == 1) return strs[0];
-
-        size_t i = 0;
-        while (true)
-        {
-            if (i >= strs[0].length()) strs[0].substr(0, i);
-
-            char t = strs[0][i];
-
-            for (size_t j = 1; j < strs.size(); ++j)
-            {
-                if (i >= strs[j].length() || strs[j][i] != t)
-                    return strs[j].substr(0, i);
+        for (int i = 0; i < strs[0].size(); ++i) {
+            char c = strs[0][i];
+            for (int j = 0; j < strs.size(); ++j) {
+                if (i >= strs[j].size() || strs[j][i] != c) {
+                    return strs[0].substr(0, i);
+                }
             }
-
-            ++i;
         }
-        return strs[0].substr(0, i);
+        return strs[0];
+    }
+};
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (strs.empty()) return "";
+        for (int i = 0; i < strs[0].size(); ++i) {
+            if (!isEqual(strs, i, 0, strs.size()-1))
+                return strs[0].substr(0, i);
+        }
+        return strs[0];
+    }
+private:
+    bool isEqual(vector<string> &strs, int index, int start, int end) {
+        if (start > end) return true;
+        if (start == end) {
+            if (strs[start].size() <= index) return false;
+            else return true;
+        }
+        int middle = start + (end - start >> 1);
+        bool left = isEqual(strs, index, start, middle);
+        bool right = isEqual(strs, index, middle+1, end);
+        if (!left || !right || strs[start].size() <= index || strs[end].size() <= index || 
+            strs[start][index] != strs[end][index]) 
+            return false;
+        return true;
     }
 };
