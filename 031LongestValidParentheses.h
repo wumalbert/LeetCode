d05695ba2ccc2ad06df598e96760ebca70299c80
wumalbert <wumalbert@gmail.com>
2013-10-24 14:36:06 +0800

    DP solution

--- 031LongestValidParentheses.h
+++ 031LongestValidParentheses.h
@@ -1,40 +1,69 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        stack<int> leftPos; // store positions of the left parenthesis
-
-        int last = -1; //record the last position of invaild parenthesis
-        int maxLength = 0;
-        for (int i = 0; i < s.length(); ++i)
-        {
-            if ('(' == s[i])
-            {
-                leftPos.push(i);
-            }
-            else
-            {
-                if (leftPos.empty()) //the string before is invaild
-                {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int count = 0, longest = 0, last = -1, left_paren = 0;
+        stack<int> left_pos;
+        for (int i = 0; i < s.length(); ++i) {
+            if ('(' == s[i]) {
+                left_pos.push(i);
+            } else {
+                if (left_pos.empty()) {
                     last = i;
-                }
-                else
-                {
-                    leftPos.pop();
-
-                    if (leftPos.empty()) //the end of this vaild string
-                    {
-                        maxLength = max(maxLength, i - last);
-                    }
-                    else //the vaild parenthesis may be longer
-                    {
-                        maxLength = max(maxLength, i - leftPos.top());
-                    }
+                } else {
+                    left_pos.pop();
+                    if (left_pos.empty()) longest = max(longest, i-last);
+                    else longest = max(longest, i-left_pos.top());
                 }
             }
         }
-
-        return maxLength;
+        return longest;
+    }
+};
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        return max(helper(s.begin(), s.end(), '('), helper(s.rbegin(), s.rend(), ')'));
+    }
+private:
+    template<class Iter>
+    int helper(Iter begin, Iter end, const char left) {
+        int longest = 0, opened = 0;
+        string::iterator last = begin-1;
+        while (begin != end) {
+            opened += *begin == '(' ? 1 : -1;
+            if (opened == 0) {
+                longest = max(longest, begin - last);
+            } else if (opened < 0) {
+                opened = 0;
+                last = begin;
+            }
+            ++begin;
+        }
+        return longest;
+    }
+};
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int len = s.length(), longest = 0;
+        vector<int> parentheses(len+1, 0);
+        for (int i = 1; i <= len; ++i) {
+            if (s[i-1] == '(') {
+                parentheses[i] = 0;
+            } else if (i >= 2){
+                if (s[i-2] == '(') {
+                    parentheses[i] = 2 + parentheses[i-2];
+                } else {
+                    int t = i-parentheses[i-1]-2;
+                    if (t >= 0 && s[t] == '(') 
+                        parentheses[i] = 2 + parentheses[i-1] + parentheses[t];
+                }
+                if (parentheses[i] > longest) longest = parentheses[i];
+            }
+        }
+        return longest;
     }
 };
