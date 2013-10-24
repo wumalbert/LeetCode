145ba6b0917a515eb5f1aeac30c6947ab217c94a
wumalbert <wumalbert@gmail.com>
2013-10-24 21:38:53 +0800

    code optimization

--- 059PermutationSequence.h
+++ 059PermutationSequence.h
@@ -1,34 +1,22 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        const static int p[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
-        
-        if (k < 1 || k > p[n]) return "";
-        
-        int kk = k-1;
-        vector<int> index(n, 0);
-        for (int i = n-1; i >= 0; --i)
-        {
-            while (kk >= p[i])
-            {
-                ++index[i];
-                kk -= p[i];
-            }
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<int> symbol(n, 1);
+        vector<int> base(n, 1);
+        for (int i = n-2, j = 1; i >= 0; --i, ++j) {
+            base[i] = j * base[i+1];
+            symbol[j-1] = '0' + j;
         }
-        
-        vector<int> num(n, 0);
-        for (int i =0; i < n; ++i)
-            num[i] = i+1;
-        
-        string ret;
-        for (int i = n-1; i >= 0; --i)
-        {
-            ret += num[index[i]] + '0';
-            num.erase(num.begin() + index[i]);
+        symbol[n-1] = '0' + n;
+        string ret(n, ' ');
+        --k;
+        for (int i = 0; i < n; ++i) {
+            int t = k / base[i];
+            ret[i] = symbol[t];
+            k -= base[i]*t;
+            symbol.erase(symbol.begin()+t);
         }
-        
         return ret;
     }
 };
