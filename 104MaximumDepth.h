a784cb123771b10104eedb40bb1cbaf11bca4ae6
wumalbert <wumalbert@gmail.com>
2013-10-25 23:32:52 +0800

    postorder traversal solution

--- 104MaximumDepth.h
+++ 104MaximumDepth.h
@@ -9,36 +9,97 @@
  */
 class Solution {
 public:
-    vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        vector<vector<int> > ret;
-        if (!root) return ret;
-        
-        queue<pair<TreeNode*, int> > levelQueue;
-        int last = -1;
-        bool forward = false; // the direction of the current level
-        levelQueue.push(make_pair(root, 0));
-        
-        while (!levelQueue.empty())
-        {
-            pair<TreeNode*, int> node = levelQueue.front();
-            levelQueue.pop();
-            
-            if (node.second != last)
-            {
-                ret.push_back(vector<int>(1, node.first->val));
-                last = node.second;
-                forward = !forward;
+    int maxDepth(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!root) return 0;
+        int left_depth = maxDepth(root->left);
+        int right_depth = maxDepth(root->right);
+        return 1 + max(left_depth, right_depth);
+    }
+};
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!root) return 0;
+        stack<TreeNode *> preorder_stack;
+        stack<int> depth_stack;
+        preorder_stack.push(NULL);
+        depth_stack.push(0);
+        TreeNode *current = root;
+        int current_depth = 1, max_depth = 0;
+        while (!preorder_stack.empty() || current) {
+            if (!current->left && !current->right && current_depth > max_depth) {
+                max_depth = current_depth;
+            } 
+            if (current->right) {
+                preorder_stack.push(current->right);
+                depth_stack.push(current_depth+1);
+            } 
+            if (current->left) {
+                current = current->left;
+                ++current_depth;
+            } else {
+                current = preorder_stack.top();
+                preorder_stack.pop();
+                current_depth = depth_stack.top();
+                depth_stack.pop();
             }
-            else
-            {
-                if (forward) ret[ret.size()-1].push_back(node.first->val);
-                else ret[ret.size()-1].insert(ret[ret.size()-1].begin(), node.first->val);
-            }
-            
-            if (node.first->left) levelQueue.push(make_pair(node.first->left, node.second+1));
-            if (node.first->right) levelQueue.push(make_pair(node.first->right, node.second+1));
         }
+        return max_depth;
+    }
+};
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!root) return 0;
+        stack<TreeNode *> postorder_stack;
+        postorder_stack.push(root);
+        int max_depth = 0;
+        TreeNode *previous = NULL, *current = NULL;
+        while (!postorder_stack.empty()) {
+            current = postorder_stack.top();
+            if (!previous || previous->left == current || previous->right == current) {
+                if (current->left) postorder_stack.push(current->left);
+                else if (current->right) postorder_stack.push(current->right);
+            } else if (current->left == previous) {
+                if (current->right) postorder_stack.push(current->right);
+            } else {
+                postorder_stack.pop();
+            }
+            previous = current;
+            if (postorder_stack.size() > max_depth) max_depth = postorder_stack.size();
+        }
+        return max_depth;
+    }
+};
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (!root) return 0;
+        queue<TreeNode *> node_queue;
+        node_queue.push(root);
+        int nodes_current_level = 1, nodes_next_level = 0, depth = 0;
+        while (!node_queue.empty()) {
+            TreeNode *node = node_queue.front();
+            node_queue.pop();
+            --nodes_current_level;
+            if (node->left) {
+                node_queue.push(node->left);
+                ++nodes_next_level;
+            }
+            if (node->right) {
+                node_queue.push(node->right);
+                ++nodes_next_level;
+            }
+            if (0 == nodes_current_level) {
+                nodes_current_level = nodes_next_level;
+                nodes_next_level = 0;
+                ++depth;
+            }
+        }
+        return depth;
     }
 };
