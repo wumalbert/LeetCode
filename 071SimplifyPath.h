fb140a9f44acbb55a3d18d97e00cadfd352485d3
wumalbert <wumalbert@gmail.com>
2013-10-25 14:41:00 +0800

    code optimization

--- 071SimplifyPath.h
+++ 071SimplifyPath.h
@@ -1,63 +1,51 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-
-        stack<string> pathStack;
-
-        int i = 0;
-        int last = -1;
-        while (i < path.length())
-        {
-            if ('/' == path[i])
-            {
-                string s = path.substr(last+1, i-last);            
-                if ("../" == s)
-                {
-                    if (pathStack.size() > 1) pathStack.pop();
-                }
-                else if ("./" != s && ("/" != s || -1 == last))
-                {
-                    pathStack.push(s);
-                }
-
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<string> paths;
+        for (int i = 0, last = -1; i <= path.size(); ++i) {
+            if (i == path.size() || path[i] == '/') {
+                if (i == 0 || i-last > 1) paths.push_back(path.substr(last+1, i-last));
                 last = i;
+            } else if (path[i] == '.') {
+                if (i + 1 == path.size()) {
+                    break;
+                } else if (path[i+1] == '.') {
+                    if ((i + 2 == path.size() || (i + 2 < path.size() && path[i+2] == '/')) && paths.size() > 1) paths.pop_back();
+                    last = i = i+2;
+                } else if (path[i+1] == '/') {
+                    last = i = i+1;
+                }
             }
-            ++i;
         }
-        if (last != path.length()-1)
-        {
-            string s = path.substr(last+1, path.length()-last);
-            if ("." != s && ".." != s)
-                pathStack.push(s);
-            else if (".." == s && pathStack.size() > 1)
-                pathStack.pop();
-        }
-
-        if (pathStack.size() <= 1) return "/";
-
-        vector<string> pathArray(pathStack.size(), "");
-
-        i = pathStack.size() - 1;
-        while (!pathStack.empty())
-        {
-            pathArray[i--] = pathStack.top();
-            pathStack.pop();
-        }
-
         string ret;
-        if ('/' != pathArray[0][0])
-            ret += "/";
-        for (i = 0; i < pathArray.size()-1; ++i)
-        {
-            ret += pathArray[i];
+        for (vector<string>::iterator it = paths.begin(); it != paths.end(); ++it)
+            ret += *it;
+        if (ret.size() > 1 && ret[ret.size()-1] == '/') ret = ret.substr(0, ret.size()-1);
+        return ret;
+    }
+};
+class Solution {
+public:
+    string simplifyPath(string path) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<string> paths;
+        for (int start = 1, end = 1; end < path.length(); ++end) {
+            for (start = end; start < path.length() && path[start] == '/'; ++start) {}
+            for (end = start+1; end < path.length() && path[end] != '/'; ++end) {}
+            string s = path.substr(start, end-start);
+            if (s.empty() || s == ".") {
+                continue;
+            } else if (s == "..") {
+                if (!paths.empty()) paths.pop_back();
+            } else {
+                paths.push_back(s); 
+            }
         }
-        if ('/' == pathArray[i][pathArray[i].size()-1])
-            ret += pathArray[i].substr(0, pathArray[i].size()-1);
-        else
-            ret += pathArray[i];
-
+        if (paths.empty()) return "/";
+        string ret;
+        for (vector<string>::iterator it = paths.begin(); it != paths.end(); ++it)
+            ret += "/" + *it;
         return ret;
     }
 };
