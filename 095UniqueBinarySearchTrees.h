63f78cb773f6cee43839054bcee78cd01465ca49
wumalbert <wumalbert@gmail.com>
2013-10-25 19:44:56 +0800

    time complexity optimization

--- 095UniqueBinarySearchTrees.h
+++ 095UniqueBinarySearchTrees.h
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int numTrees(int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        
-        vector<int> nBST(n+1, 1);
-        
-        for (int i = 2; i <= n; ++i)
-        {
-            nBST[i] = 0;
-            for (int j = 1; j <= i; ++j)
-            {
-                nBST[i] += nBST[j-1]*nBST[i-j];
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        vector<int> trees(n+1, 0);
+        trees[0] = trees[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                trees[i] += trees[j-1] * trees[i-j];
             }
         }
-        
-        return nBST[n];
+        return trees[n];
+    }
+};
+class Solution {
+public:
+    int numTrees(int n) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int number = 1;
+        for (int i = 1; i <= n; ++i) {
+            number = number * 2 * (2*i - 1) / (i+1);
+        }
+        return number;
     }
 };
