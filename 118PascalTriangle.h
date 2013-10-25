1a8c43b48b7d3d622d038659fa298709964d10a5
wumalbert <wumalbert@gmail.com>
2013-10-26 00:31:25 +0800

    code optimization

--- 118PascalTriangle.h
+++ 118PascalTriangle.h
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<vector<int> > generate(int numRows) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
+        // Note: The Solution object is instantiated only once and is reused by each test case.
         vector<vector<int> > ret;
-        
-        for (int i = 1; i <= numRows; ++i)
-        {
-            ret.push_back(vector<int>(i, 1));
-            
-            for (int j = 1; j < i-1; ++j)
-            {
-                ret[i-1][j] = ret[i-2][j-1] + ret[i-2][j];
+        if (numRows <= 0) return ret;
+        ret.resize(numRows, vector<int>());
+        ret[0].push_back(1);
+        for (int i = 1; i < numRows; ++i) {
+            ret[i].resize(i+1, 1);
+            for (int j = 1; j < i; ++j) {
+                ret[i][j] = ret[i-1][j-1] + ret[i-1][j];
             }
         }
-        
         return ret;
     }
 };
