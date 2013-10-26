085211a5102ff5c73f71556068c3e142d11cfc17
wumalbert <wumalbert@gmail.com>
2013-10-26 16:18:38 +0800

    general solution

--- 123BestTimeToBuyAndSellStock3.h
+++ 123BestTimeToBuyAndSellStock3.h
@@ -1,50 +1,42 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (prices.empty()) return 0;
-        int len = prices.size();
-        // firstProfit[i]: maximum profit of prices[0, i)
-        vector<int> firstProfit(len+1, 0);
-        int low = prices[0], high = prices[0];
-        for (int i = 1; i < len; ++i)
-        {
-            if (prices[i] < low)
-            {
-                high = low = prices[i];
-            }
-            else if (prices[i] > high)
-            {
-                high = prices[i];
-            }
-            firstProfit[i+1] = max(firstProfit[i], high - low);
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (prices.size() < 2) return 0;
+        int n = prices.size(), bought = prices[0], max_profit = 0;
+        vector<int> first_profit(n, 0), second_profit(n, 0);
+        for (int i = 1; i < n; ++i) { // [0, i]
+            if (prices[i] - bought > max_profit) max_profit = prices[i] - bought;
+            first_profit[i] = max_profit;
+            if (prices[i] < bought) bought = prices[i];
         }
-        
-        // secondProfit[i]: maximum profit of prices [i, end)
-        vector<int> secondProfit(len+1, 0);
-        low = high = prices[len-1];
-        for (int i = len-2; i >= 0; --i)
-        {
-            if (prices[i] < low)
-            {
-                low = prices[i];
-            }
-            else if (prices[i] > high)
-            {
-                low = high = prices[i];
-            }
-            secondProfit[i] = max(secondProfit[i+1], high - low);
+        int sell = prices[n-1], ret = 0;
+        max_profit = 0;
+        for (int i = n-1; i >= 0; --i) { // [i, n)
+            if (sell - prices[i]> max_profit) max_profit = sell - prices[i];
+            second_profit[i] = max_profit;
+            if (first_profit[i] + second_profit[i] > ret)
+                ret = first_profit[i] + second_profit[i];
+            if (prices[i] > sell) sell = prices[i];
         }
-        
-        // use dynamic programming to find the maximum profit
-        int profit = numeric_limits<int>::min();
-        for (int i = 0; i <= len; ++i)
-        {
-            if (firstProfit[i] + secondProfit[i] > profit)
-                profit = firstProfit[i] + secondProfit[i];
+        return ret;
+    }
+};
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (prices.size() < 2) return 0;
+        int n = prices.size();
+        vector<int> f(n+1, 0), g(n+1, 0);
+        for (int i = 0; i + 1 < n; ++i) {
+            int diff = prices[i+1] - prices[i];
+            int m = min(i+1, 2);
+            for (int j = m; j > 0; --j) {
+                f[j] = max(f[j], g[j-1]) + diff;
+                g[j] = max(f[j], g[j]);
+            }
         }
-        
-        return profit;
+        return max(g[1], g[2]);
     }
 };
