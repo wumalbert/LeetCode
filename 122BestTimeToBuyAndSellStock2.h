82cadf0550e5ad82894461be4f910295d19886fd
wumalbert <wumalbert@gmail.com>
2013-10-26 11:13:50 +0800

    code optimization

--- 122BestTimeToBuyAndSellStock2.h
+++ 122BestTimeToBuyAndSellStock2.h
@@ -1,32 +1,38 @@
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (prices.empty()) return 0;
-        // buy at minmum point and sell at maximum point
-        int len = prices.size(), low = prices[0], profit = 0;
-        bool bought = true; // 
-        for (int i = 1; i < len-1; ++i)
-        {
-            // maxmum point
-            if (prices[i] > prices[i-1] && prices[i] >= prices[i+1])
-            {
-                if (bought)
-                {
-                    profit += prices[i] - low;
-                    bought = false;
-                }
-            }
-            else if (prices[i] <= prices[i-1] && prices[i] < prices[i+1])
-            {
-                low = prices[i];
-                bought = true;
-            }
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int bought = 0, sell = -1, ret = 0;
+        while (sell + 2 < prices.size()) {
+            for (bought = sell+1; bought+1 < prices.size() && prices[bought] >= prices[bought+1]; ++bought) {}
+            for (sell = bought+1; sell+1 < prices.size() && prices[sell] <= prices[sell+1]; ++sell) {}
+            if (sell < prices.size()) ret += prices[sell] - prices[bought];
         }
-        if (bought && prices[len-1] > low)
-            profit += prices[len-1] - low;
-        
-        return profit;
+        return ret;
+    }
+};
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int bought = 0, sell = -1, ret = 0;
+        for (int i = 0; i < prices.size(); ++i) {
+            if (i > 0 && prices[i] > prices[i-1])
+                ret += prices[i] - prices[i-1];
+        }
+        return ret;
+    }
+};
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int ret = 0;
+        for (int i = 0, j = 0; i + 1 < prices.size(); ++i) {
+            for (j = i+1; j < prices.size() && prices[j-1] <= prices[j]; ++j) {}
+            ret += prices[j-1] - prices[i];
+            i = j-1;
+        }
+        return ret;
     }
 };
