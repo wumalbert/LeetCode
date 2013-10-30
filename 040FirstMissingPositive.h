d1603b81401c848fb962f4e0a62cebd704a64d68
wumalbert <wumalbert@gmail.com>
2013-10-30 16:50:52 +0800

    code optimization

--- 040FirstMissingPositive.h
+++ 040FirstMissingPositive.h
@@ -1,26 +1,22 @@
 class Solution {
+private:
+    void swap(int &a, int &b) {
+        if (a != b) {
+            a = a^b;
+            b = a^b;
+            a = a^b;
+        }
+    }
 public:
     int firstMissingPositive(int A[], int n) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        for (int i = 0; i < n; ++i)
-        {
-            if (A[i] > 0 && A[i] < n)
-            {
-                int t = A[i]-1;
-                if (t != i && A[t] != A[i])
-                {
-                    A[t] = A[t] ^ A[i];
-                    A[i] = A[t] ^ A[i];
-                    A[t] = A[t] ^ A[i];
-                    --i;
-                }
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        for (int i = 0; i < n; ++i) {
+            if (A[i] > 0 && A[i] != i+1 && A[i] <= n && A[A[i]-1] != A[i]) {
+                swap(A[i], A[A[i]-1]);
+                --i;
             }
-            
         }
-        
-        for (int i = 0; i < n; ++i)
-        {
+        for (int i = 0; i < n; ++i) {
             if (A[i] != i+1) return i+1;
         }
         return n+1;
